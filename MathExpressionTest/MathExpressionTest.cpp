841d2c5303f33520fbfa7b217f5074f0896e838e
terrakuh <greybley@yahoo.de>
2018-03-04 17:23:17 +0100

    Add expression test

--- MathExpressionTest/MathExpressionTest.cpp
+++ MathExpressionTest/MathExpressionTest.cpp
@@ -20,11 +20,36 @@ namespace MathExpressionTest
 		TEST_METHOD(TestDotFirstConstant)
 		{
 			auto szScript = "global test_dot_first_constant = 652 - 2 + 6956 * 998 / 53;";
-
+			
 			m_context.Run(szScript, std::char_traits<char>::length(szScript));
 
 			//Get variable
 			auto pVariable = m_context.GetGlobal("test_dot_first_constant");
+
+			Assert::IsNotNull(pVariable, L"Variable not found.");
+
+			//Get value
+			auto pValue = pVariable->Cast<int>();
+
+			Assert::IsNotNull(pValue, L"Invalid type.");
+
+			//Test value
+			Assert::AreEqual(*pValue, 652 - 2 + 6956 * 998 / 53);
+		}
+
+		TEST_METHOD(TestDotFirstMixed)
+		{
+			auto szScript = R"(
+global a = 652;
+global b = 6956;
+global c = 998;
+global d = 53;
+global test_dot_first_mixed = a - 2 + b * c / d;)";
+
+			m_context.Run(szScript, std::char_traits<char>::length(szScript));
+
+			//Get variable
+			auto pVariable = m_context.GetGlobal("test_dot_first_mixed");
 
 			Assert::IsNotNull(pVariable, L"Variable not found.");
 
