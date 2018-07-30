2b47a5c2faa3e9ca4e4be63ecd18554e56b40e47
terrakuh <greybley@yahoo.de>
2018-07-30 10:16:03 +0200

    Add return to add_instruction
    architecture::add_instruction now returns the size of the instruction in bytes.

--- Bia/architecture.hpp
+++ Bia/architecture.hpp
@@ -85,9 +85,11 @@ public:
 	 * @param [out] _output The output stream.
 	 *
 	 * @throws See stream::output_stream::write_all().
+	 *
+	 * @return The size of the instruction in bytes.
 	*/
 	template<OP_CODE _Op_code>
-	static void add_instruction(stream::output_stream & _output)
+	static size_t add_instruction(stream::output_stream & _output)
 	{
 		static_assert(_Op_code == OP_CODE::LEAVE || _Op_code == OP_CODE::RETURN_NEAR, "This opcode is not supported.");
 
@@ -110,9 +112,11 @@ public:
 	 * @param [out] _output The output stream.
 	 *
 	 * @throws See stream::output_stream::write_all().
+	 *
+	 * @return The size of the instruction in bytes.
 	*/
 	template<OP_CODE _Op_code, REGISTER _Register>
-	static void add_instruction(stream::output_stream & _output)
+	static size_t add_instruction(stream::output_stream & _output)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH || _Op_code == OP_CODE::CALL, "This opcode is not supported.");
 
@@ -138,9 +142,11 @@ public:
 	 * @param _offset The offset.
 	 *
 	 * @throws See stream::output_stream::write_all().
+	 *
+	 * @return The size of the instruction in bytes.
 	*/
 	template<OP_CODE _Op_code, REGISTER _Dest, REGISTER _Src, typename _Offset>
-	static void add_instruction(stream::output_stream & _output, _Offset _offset)
+	static size_t add_instruction(stream::output_stream & _output, _Offset _offset)
 	{
 		static_assert(_Op_code == OP_CODE::MOVE || _Op_code == OP_CODE::LEA || _Op_code == OP_CODE::TEST, "This opcode is not supported.");
 		static_assert(std::is_same<int8_t, _Offset>::value || std::is_same<int32_t, _Offset>::value, "Offset must be int8_t or int32_t.");
@@ -154,25 +160,25 @@ public:
 			} // 8 bit offset
 			else if (std::is_same<_Offset, int8_t>::value) {
 				return _output.write_all(0x8b_8, static_cast<uint8_t>(0100 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint8_t>(_offset));
-			} // 32 bit offset
-			else {
-				return _output.write_all(0x8b_8, static_cast<uint8_t>(0200 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint32_t>(_offset));
 			}
+
+			// 32 bit offset
+			return _output.write_all(0x8b_8, static_cast<uint8_t>(0200 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint32_t>(_offset));
 		}
 		case OP_CODE::LEA:
 		{
 			// Add SIB byte
 			if (_Src == REGISTER::ESP) {
 				static_assert(!(_Src == REGISTER::ESP && _Op_code == OP_CODE::LEA), "SIB not implemented.");
-			} else {
-				// 8 bit offset
-				if (std::is_same<int8_t, _Offset>::value) {
-					return _output.write_all(0x8d_8, static_cast<uint8_t>(0100 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint8_t>(_offset));
-				} // 32 bit offset
-				else {
-					return _output.write_all(0x8d_8, static_cast<uint8_t>(0200 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint32_t>(_offset));
-				}
 			}
+
+			// 8 bit offset
+			if (std::is_same<int8_t, _Offset>::value) {
+				return _output.write_all(0x8d_8, static_cast<uint8_t>(0100 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint8_t>(_offset));
+			} 
+				
+			// 32 bit offset
+			return _output.write_all(0x8d_8, static_cast<uint8_t>(0200 | get_register_code<_Src>() | get_register_code<_Dest>() << 3), static_cast<uint32_t>(_offset));
 		}
 		case OP_CODE::TEST:
 		{
@@ -181,15 +187,15 @@ public:
 			// 8 bit register
 			if (RegisterSize<_Dest>() == 8) {
 				return _output.write_all(0x84_8, static_cast<uint8_t>(0300 | get_register_code<_Src>() << 3 | get_register_code<_Dest>()));
-			} // 32 bit register
-			else {
-				return _output.write_all(0x85_8, static_cast<uint8_t>(0300 | get_register_code<_Src>() << 3 | get_register_code<_Dest>()));
 			}
+			
+			// 32 bit register
+			return _output.write_all(0x85_8, static_cast<uint8_t>(0300 | get_register_code<_Src>() << 3 | get_register_code<_Dest>()));
 		}
 		}
 	}
 	template<OP_CODE _Op_code, REGISTER _Register, typename _Offset>
-	static void add_instruction(stream::output_stream & _output, _Offset _offset)
+	static size_t add_instruction(stream::output_stream & _output, _Offset _offset)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH, "This opcode is not supported.");
 		static_assert(std::is_same<int8_t, _Offset>::value || std::is_same<int32_t, _Offset>::value, "Offset must be int8_t or int32_t.");
@@ -199,14 +205,14 @@ public:
 		{
 			if (std::is_same<int8_t, _Offset>::value) {
 				return _output.write_all(0xff_8, static_cast<uint8_t>(0160 | get_register_code<_Register>()), _offset);
-			} else {
-				return _output.write_all(0xff_8, static_cast<uint8_t>(0260 | get_register_code<_Register>()), _offset);
 			}
+
+			return _output.write_all(0xff_8, static_cast<uint8_t>(0260 | get_register_code<_Register>()), _offset);
 		}
 		}
 	}
 	template<OP_CODE _Op_code>
-	static void add_instruction32(stream::output_stream & _output, int32_t _value)
+	static size_t add_instruction32(stream::output_stream & _output, int32_t _value)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH || _Op_code == OP_CODE::JUMP_RELATIVE || _Op_code == OP_CODE::JUMP_EQUAL || _Op_code == OP_CODE::JUMP_NOT_EQUAL, "This opcode is not supported.");
 
@@ -222,7 +228,7 @@ public:
 		}
 	}
 	template<OP_CODE _Op_code>
-	static void add_instruction8(stream::output_stream & _output, int8_t _value)
+	static size_t add_instruction8(stream::output_stream & _output, int8_t _value)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH || _Op_code == OP_CODE::JUMP_RELATIVE || _Op_code == OP_CODE::JUMP_EQUAL || _Op_code == OP_CODE::JUMP_NOT_EQUAL, "This opcode is not supported.");
 
@@ -238,7 +244,7 @@ public:
 		}
 	}
 	template<OP_CODE _Op_code, REGISTER _Register>
-	static void add_instruction32(stream::output_stream & _output, int32_t _value)
+	static size_t add_instruction32(stream::output_stream & _output, int32_t _value)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH || _Op_code == OP_CODE::MOVE || _Op_code == OP_CODE::ADD || _Op_code == OP_CODE::SUBTRACT, "This opcode is not supported.");
 
@@ -252,23 +258,23 @@ public:
 			// Special opcode for EAX
 			if (_Register == REGISTER::EAX) {
 				return _output.write_all(0x05_8, static_cast<uint32_t>(_value));
-			} else {
-				return _output.write_all(0x81_8, static_cast<uint8_t>(0300 | get_register_code<_Register>()), static_cast<uint32_t>(_value));
 			}
+			
+			return _output.write_all(0x81_8, static_cast<uint8_t>(0300 | get_register_code<_Register>()), static_cast<uint32_t>(_value));
 		}
 		case OP_CODE::SUBTRACT:
 		{
 			// Special opcode for EAX
 			if (_Register == REGISTER::EAX) {
 				return _output.write_all(0x2d_8, static_cast<uint32_t>(_value));
-			} else {
-				return _output.write_all(0x81_8, static_cast<uint8_t>(0350 | get_register_code<_Register>()), static_cast<uint32_t>(_value));
 			}
+			
+			return _output.write_all(0x81_8, static_cast<uint8_t>(0350 | get_register_code<_Register>()), static_cast<uint32_t>(_value));
 		}
 		}
 	}
 	template<OP_CODE _Op_code, REGISTER _Register>
-	static void add_instruction8(stream::output_stream & _output, int8_t _value)
+	static size_t add_instruction8(stream::output_stream & _output, int8_t _value)
 	{
 		static_assert(_Op_code == OP_CODE::PUSH || _Op_code == OP_CODE::ADD || _Op_code == OP_CODE::SUBTRACT, "This opcode is not supported.");
 
@@ -278,27 +284,27 @@ public:
 			// If constant displacement is 0 the push register directly, otherwise push with one byte displacement
 			if (!_value) {
 				return add_instruction<OP_CODE::PUSH, _Register>(_output);
-			} else {
-				return _output.write_all(0xff_8, static_cast<uint8_t>(0160 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 			}
+
+			return _output.write_all(0xff_8, static_cast<uint8_t>(0160 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 		}
 		case OP_CODE::ADD:
 		{
 			// Special opcode for EAX
 			if (_Register == REGISTER::EAX) {
 				return _output.write_all(0x04_8, static_cast<uint8_t>(_value));
-			} else {
-				return _output.write_all(0x83_8, static_cast<uint8_t>(0300 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 			}
+
+			return _output.write_all(0x83_8, static_cast<uint8_t>(0300 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 		}
 		case OP_CODE::SUBTRACT:
 		{
 			// Special opcode for EAX
 			if (_Register == REGISTER::EAX) {
 				return _output.write_all(0x2c_8, static_cast<uint8_t>(_value));
-			} else {
-				return _output.write_all(0x83_8, static_cast<uint8_t>(0350 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 			}
+			
+			return _output.write_all(0x83_8, static_cast<uint8_t>(0350 | get_register_code<_Register>()), static_cast<uint8_t>(_value));
 		}
 		}
 	}
