f7ab8715e101b04319fe08026a263d0b74c646c6
terrakuh <greybley@yahoo.de>
2018-08-03 17:39:43 +0200

    Add serialize methods

--- Bia/report.hpp
+++ Bia/report.hpp
@@ -5,6 +5,8 @@
 
 #include "operator.hpp"
 #include "interpreter_string.hpp"
+#include "output_stream.hpp"
+#include "input_stream.hpp"
 
 
 namespace bia
@@ -64,6 +66,76 @@ struct report
 		framework::operator_type operator_code;
 		INTERPRETER_STRING keyword;
 	} content;
+
+
+	void serialize(stream::output_stream & _output)
+	{
+		_output.write_all<uint32_t, uint32_t>(static_cast<uint32_t>(type) << custom_bits | custom_parameter, rule_id << token_bits | token_id);
+		
+		switch (type) {
+		case TYPE::STRING:
+		case TYPE::INT_VALUE:
+			_output.write_all(content.int_value);
+
+			break;
+		case TYPE::DOUBLE_VALUE:
+			_output.write_all(content.double_value);
+
+			break;
+		case TYPE::MEMBER:
+		case TYPE::OPERATOR_CODE:
+			_output.write_all(content.operator_code);
+
+			break;
+		case TYPE::KEYWORD:
+			_output.write_all(content.keyword);
+
+			break;
+		case TYPE::BEGIN:
+		case TYPE::END:
+		case TYPE::EMPTY_CHILD:
+		default:
+			throw;
+		}
+	}
+	void deserialize(stream::input_stream & _input)
+	{
+		auto _tmp = _input.read<uint32_t>();
+
+		type = static_cast<TYPE>(_tmp >> custom_bits);
+		custom_parameter = _tmp & ~uint32_t(0) << custom_bits;
+
+		_tmp = _input.read<uint32_t>();
+
+		rule_id = _tmp >> token_bits;
+		token_id = _tmp & ~uint32_t(0) << token_bits;
+
+		switch (type) {
+		case TYPE::STRING:
+		case TYPE::INT_VALUE:
+			content.int_value = _input.read<int64_t>();
+
+			break;
+		case TYPE::DOUBLE_VALUE:
+			content.double_value = _input.read<double>();
+
+			break;
+		case TYPE::MEMBER:
+		case TYPE::OPERATOR_CODE:
+			content.operator_code = _input.read<framework::operator_type>();
+
+			break;
+		case TYPE::KEYWORD:
+			content.keyword = _input.read<INTERPRETER_STRING>();
+
+			break;
+		case TYPE::BEGIN:
+		case TYPE::END:
+		case TYPE::EMPTY_CHILD:
+		default:
+			throw;
+		}
+	}
 };
 
 }
