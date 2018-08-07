85a029ea074331b95328468277ad8c29b2d20dfc
terrakuh <greybley@yahoo.de>
2018-08-07 11:48:35 +0200

    Change return type of constructor_chain

--- Bia/constructor_chain.hpp
+++ Bia/constructor_chain.hpp
@@ -21,24 +21,24 @@ struct constructor_chain;
 template<typename _Ty>
 struct constructor_chain<_Ty, true, true>
 {
-	static _Ty * construct(machine::memory::allocator * _allocator, _Ty && _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, _Ty && _object)
 	{
-		return _allocator->construct<_Ty>(std::move(_object)).first;
+		return _allocator->construct<_Ty>(std::move(_object));
 	}
-	static _Ty * construct(machine::memory::allocator * _allocator, const _Ty & _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, const _Ty & _object)
 	{
-		return _allocator->construct<_Ty>(_object).first;
+		return _allocator->construct<_Ty>(_object);
 	}
 };
 
 template<typename _Ty>
 struct constructor_chain<_Ty, true, false>
 {
-	static _Ty * construct(machine::memory::allocator * _allocator, _Ty && _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, _Ty && _object)
 	{
-		return _allocator->construct<_Ty>(std::move(_object)).first;
+		return _allocator->construct<_Ty>(std::move(_object));
 	}
-	static _Ty * construct(machine::memory::allocator * _allocator, const _Ty & _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, const _Ty & _object)
 	{
 		throw;
 	}
@@ -47,37 +47,37 @@ struct constructor_chain<_Ty, true, false>
 template<typename _Ty>
 struct constructor_chain<_Ty, false, true>
 {
-	static _Ty * construct(machine::memory::allocator * _allocator, _Ty && _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, _Ty && _object)
 	{
-		return _allocator->construct<_Ty>(_object).first;
+		return _allocator->construct<_Ty>(_object);
 	}
-	static _Ty * construct(machine::memory::allocator * _allocator, const _Ty & _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, const _Ty & _object)
 	{
-		return _allocator->construct<_Ty>(_object).first;
+		return _allocator->construct<_Ty>(_object);
 	}
 };
 
 template<typename _Ty>
 struct constructor_chain<_Ty, false, false>
 {
-	static _Ty * construct(machine::memory::allocator * _allocator, _Ty && _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, _Ty && _object)
 	{
 		throw;
 	}
-	static _Ty * construct(machine::memory::allocator * _allocator, const _Ty & _object)
+	static machine::memory::allocator::allocation<_Ty> construct(machine::memory::allocator * _allocator, const _Ty & _object)
 	{
 		throw;
 	}
 };
 
 template<typename _Ty>
-_Ty * constructor_chain_wrapper(machine::memory::allocator * _allocator, _Ty && _object)
+machine::memory::allocator::allocation<_Ty> constructor_chain_wrapper(machine::memory::allocator * _allocator, _Ty && _object)
 {
 	return constructor_chain<_Ty, std::is_move_constructible<_Ty>::value, std::is_copy_constructible<_Ty>::value>::construct(_allocator, std::move(_object));
 }
 
 template<typename _Ty>
-_Ty * constructor_chain_wrapper(machine::memory::allocator * _allocator, const _Ty & _object)
+machine::memory::allocator::allocation<_Ty> constructor_chain_wrapper(machine::memory::allocator * _allocator, const _Ty & _object)
 {
 	return constructor_chain<_Ty, std::is_move_constructible<_Ty>::value, std::is_copy_constructible<_Ty>::value>::construct(_allocator, _object);
 }
