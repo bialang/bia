5ecf24e82e37e91b449876f9783a144b5d23af82
terrakuh <greybley@yahoo.de>
2018-10-26 11:12:07 +0200

    Update modular docs

--- Bia/module_library.hpp
+++ Bia/module_library.hpp
@@ -28,15 +28,49 @@ class machine_context;
 namespace modular
 {
 
+/**
+ * @brief A module interface.
+ *
+ * Every module implements this interface. This interface enables the loading of a module.
+ *
+ * @see @ref module_loader
+*/
 class module_library
 {
 public:
-  virtual ~module_library() = default;
-  virtual int32_t load_all(bia::machine::machine_context * _context, bia::framework::member * _destination) = 0;
-  virtual int32_t version() = 0;
+	/**
+	 * Destructor.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	*/
+	virtual ~module_library() = default;
+	/**
+	 * Loads the whole library.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @param [in] _context The machine context of the script.
+	 * @param [out] _destination The destination of the library.
+	 *
+	 * @return 0 if it succeeded, otherwise an error code.
+	*/
+	virtual int32_t load_all(bia::machine::machine_context * _context, bia::framework::member * _destination) = 0;
+	/**
+	 * Returns the version of the library.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @return The version of the library.
+	*/
+	virtual int32_t version() noexcept = 0;
 };
 
+/** The module loader function signature. */
 typedef module_library*(*module_loader_signature)(memory::allocator*);
+/** The module unloader function signature. */
 typedef void(*module_unloader_signature)(memory::allocator*, module_library*);
 
 }
--- Bia/module_loader.hpp
+++ Bia/module_loader.hpp
@@ -12,18 +12,67 @@ namespace machine
 namespace modular
 {
 
+/**
+ * @brief The module loader.
+ *
+ * @see @ref module_library
+*/
 class module_loader
 {
 public:
-	BIA_EXPORT module_loader(memory::allocator * _allocator);
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @param [in] _allocator The memory allocator of the context.
+	*/
+	BIA_EXPORT module_loader(memory::allocator * _allocator) noexcept;
 	module_loader(const module_loader & _copy) = delete;
-	BIA_EXPORT module_loader(module_loader && _move) noexcept;
-	BIA_EXPORT ~module_loader();
-
+	/**
+	 * Move-Constructor.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @param [in,out] _move The object that should be moved.
+	*/
+	module_loader(module_loader && _move) noexcept = default;
+	/**
+	 * Destructor.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	*/
+	BIA_EXPORT ~module_loader() noexcept;
+	/**
+	 * Unloads a module.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @param [in] _module The module. After this operation the module is invalid.
+	 *
+	 * @todo Implement function.
+	*/
 	BIA_EXPORT void unload_module(module_library * _module);
+	/**
+	 * Loads a BLL module.
+	 *
+	 * @since 3.68.140.789
+	 * @date 26-Oct-18
+	 *
+	 * @param _filepath The directory where the module is located.
+	 *
+	 * @todo Add exceptions for errors.
+	 *
+	 * @return The loaded module if it succeeded.
+	*/
 	BIA_EXPORT module_library * load_bll(const char * _filepath, const char * _name);
 
 private:
+	/** The memory allocator of the context. */
 	memory::allocator * _allocator;
 };
 
