60937f0933cbf50b1bbb0eb8968e628248a23537
Yunus Ayar <greybley@yahoo.de>
2018-11-30 20:02:58 +0100

    Update block_allocator.hpp

--- Bia/block_allocator.hpp
+++ Bia/block_allocator.hpp
@@ -12,37 +12,24 @@ namespace machine
 namespace memory
 {
 
-template<size_type... Block_sizes>
-class block_allocator : public allocator
+template<size_type Block_size0>
+class block_allocator0 : public allocator
 {
 public:
-	/** The count of available block sizes. */
-	constexpr static size_t block_count = sizeof...(Block_sizes);
+	constexpr static auto block_size0 = Block_size0;
 
-	template<size_type Block_size>
-	void deallocate_block(universal_allocation _block)
-	{
-		deallocate_block(universal_allocation _block, Block_size, block_size_id<Block_size, Block_sizes...>());
-	}
-	template<size_type Block_size>
-	universal_allocation allocate_block()
-	{
-		return allocate_block(Block_size, block_size_id<Block_size, Block_sizes...>());
-	}
-protected:
-	virtual void deallocate_block(universal_allocation _block, size_type _block_size, size_t _block_id) = 0;
-	virtual universal_allocation allocate_block(size_type _block_size, size_t _block_id) = 0;
-	template<size_type Size, size_type Block_size, size_type... Remaining_block_sizes>
-	constexpr static typename std::enable_if<Size == Block_size, size_t>::type block_size_id() noexcept
-	{
-		return 0;
-	}
-	template<size_type Size, size_type Block_size, size_type... Remaining_block_sizes>
-	constexpr static typename std::enable_if<(Size != Block_size && sizeof...(Remaining_block_sizes) > 0), size_t>::type block_size_id() noexcept
-	{
-		return block_size_id<Size, Remaining_block_sizes...>() + 1;
-	}
-private:
+	virtual void deallocate_block0(universal_allocation _block) = 0;
+	virtual universal_allocation allocate_block0() = 0;
+};
+
+template<size_type Block_size0, size_type Block_size1>
+class block_allocator1 : public block_allocator0<block_allocator0>
+{
+public:
+	constexpr static auto block_size1 = Block_size1;
+
+	virtual void deallocate_block1(universal_allocation _block) = 0;
+	virtual universal_allocation allocate_block1() = 0;
 };
 
 }
