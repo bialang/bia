47026d6ab7334962d5ffa3cd14276b8cd487c740
Yunus Ayar <greybley@yahoo.de>
2018-11-30 23:57:43 +0100

    Seperate object definitions and declarations

---
+++ Bia/object_def.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <utility>
+
+#include "object_variable.hpp"
+#include "share_def.hpp"
+#include "member_map_def.hpp"
+#include "instance_holder.hpp"
+
+
+namespace bia
+{
+namespace framework
+{
+namespace object
+{
+
+template<typename Type>
+class object : public object_variable
+{
+public:
+	typedef utility::share<std::pair<instance_holder<Type>, member_map>> data_type;
+
+	/**
+	 * Constructor.
+	 *
+	 * @remarks A new share will be created for the object.
+	 *
+	 * @since 3.67.135.753
+	 * @date 7-Aug-18
+	 *
+	 * @param _instance The instance.
+	 * @param _members The known members.
+	*/
+	object(const instance_holder<Type> & _instance, const member_map & _members) noexcept;
+	/**
+	 * Refer-Constructor.
+	 *
+	 * @since 3.67.135.753
+	 * @date 7-Aug-18
+	 *
+	 * @param _data The data.
+	*/
+	object(const data_type & _data) noexcept;
+	virtual void BIA_MEMBER_CALLING_CONVENTION print() const override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION copy(member * _destination) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION refer(member * _destination) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION execute(member * _destination) override;
+	virtual void BIA_VARG_MEMBER_CALLING_CONVENTION execute_count(member * _destination, void * _reserved, parameter_count _count...) override;
+	virtual void BIA_VARG_MEMBER_CALLING_CONVENTION execute_format(member * _destination, const char * _format, parameter_count _count...) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION operator_call(member * _destination, operator_type _operator, const member * _right) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION operator_call_int32(member * _destination, operator_type _operator, int32_t _right) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION operator_call_int64(member * _destination, operator_type _operator, int64_t _right) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION operator_call_double(member * _destination, operator_type _operator, double _right) override;
+	virtual void BIA_MEMBER_CALLING_CONVENTION object_member(member * _destination, machine::string_manager::name_type _name) override;
+	virtual int flags() const override;
+	virtual int32_t BIA_MEMBER_CALLING_CONVENTION test() const override;
+	virtual int32_t BIA_MEMBER_CALLING_CONVENTION test_member(operator_type _operator, member * _right) const override;
+	virtual int32_t BIA_MEMBER_CALLING_CONVENTION test_int32(operator_type _operator, int32_t _right) const override;
+	virtual int32_t BIA_MEMBER_CALLING_CONVENTION test_int64(operator_type _operator, int64_t _right) const override;
+	virtual int32_t BIA_MEMBER_CALLING_CONVENTION test_double(operator_type _operator, double _right) const override;
+	virtual int64_t to_int() const override;
+	virtual double to_double() const override;
+
+protected:
+	virtual void * native_data(native::NATIVE_TYPE _type) override;
+	virtual const void * const_native_data(native::NATIVE_TYPE _type) const override;
+	virtual void * data(const std::type_info & _type) override;
+	virtual const void * const_data(const std::type_info & _type) const override;
+
+private:
+	data_type _data;
+};
+
+}
+}
+}
