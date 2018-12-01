1eeaf9a914848679107d18c2357cbeeba5ff3e57
Yunus Ayar <greybley@yahoo.de>
2018-12-01 11:29:17 +0100

    Update mpir_allocator

--- Bia/mpir_allocator.cpp
+++ Bia/mpir_allocator.cpp
@@ -15,6 +15,11 @@ namespace memory
 std::shared_ptr<allocator> mpir_allocator::_allocator;
 
 
+void mpir_allocator::int_destructor_function(universal_allocation _int)
+{
+	mpz_clear(cast_allocation<int_type>(_int));
+}
+
 void bia::machine::memory::mpir_allocator::initialize(const std::shared_ptr<allocator>& _allocator)
 {
 	mpir_allocator::_allocator = _allocator;
@@ -29,11 +34,15 @@ void mpir_allocator::free(void * _ptr, size_t _size)
 
 void mpir_allocator::free_int(int_type * _int)
 {
+	mpz_clear(_int);
+
 	_allocator->deallocate(universal_allocation(_int, sizeof(int_type) + sizeof(*int_type::_mp_d) * 2));
 }
 
 void mpir_allocator::free_double(double_type * _double)
 {
+	mpf_clear(_double);
+
 	_allocator->deallocate(universal_allocation(_double, sizeof(double_type) + sizeof(*double_type::_mp_d) * 2));
 }
 
@@ -57,15 +66,15 @@ void * mpir_allocator::reallocate(void * _buffer, size_t _old_size, size_t _new_
 	return _allocator->reallocate(universal_allocation(_buffer, _old_size), _new_size);
 }
 
-mpir_allocator::int_type * mpir_allocator::new_int()
+allocation<mpir_allocator::int_type> mpir_allocator::new_int()
 {
-	auto _int = static_cast<int_type*>(_allocator->allocate(sizeof(int_type) + sizeof(*int_type::_mp_d) * 2).first);
+	auto _int = cast_allocation<int_type>(_allocator->allocate(sizeof(int_type) + sizeof(*int_type::_mp_d) * 2));
 	
 	// Set
 	memset(_int, 0, sizeof(int_type));
 
 	_int->_mp_alloc = 2;
-	_int->_mp_d = reinterpret_cast<decltype(int_type::_mp_d)>(_int + 1);
+	_int->_mp_d = reinterpret_cast<decltype(int_type::_mp_d)>(_int.first + 1);
 
 	return _int;
 }
