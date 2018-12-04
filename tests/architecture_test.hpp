0beb43cdf650ad82591a6d94d2e58fbf1fee8de1
Yunus Ayar <greybley@yahoo.de>
2018-12-04 14:19:50 +0100

    Update testing

--- tests/architecture_test.hpp
+++ tests/architecture_test.hpp
@@ -1,23 +1,12 @@
 #pragma once
 
-#include <architecture.cpp>
-#include <cppunit/extensions/HelperMacros.h>
+#include <architecture.hpp>
+
+#include "test.hpp"
 
 
-class architecture_test : public CPPUNIT_NS::TestFixture
+class architecture_test
 {
-	CPPUNIT_TEST_SUITE(architecture_test);
-	CPPUNIT_TEST(test_move);
-	CPPUNIT_TEST(test_add);
-	CPPUNIT_TEST(test_sub);
-	CPPUNIT_TEST(test_xor);
-	CPPUNIT_TEST(test_jump);
-	CPPUNIT_TEST(test_push);
-	CPPUNIT_TEST(test_pop);
-	CPPUNIT_TEST(test_function_call);
-	CPPUNIT_TEST(test_other);
-	CPPUNIT_TEST_SUITE_END();
-	
 public:
 	void test_move();
 	void test_add();
--- tests/test.hpp
+++ tests/test.hpp
@@ -1,49 +1,109 @@
 #pragma once
 
-#include <biaMachineContext.hpp>
-#include <biaMember.hpp>
+#include <map>
 #include <cstring>
 #include <exception>
+#include <typeinfo>
+#include <cstdarg>
+#include <utility>
+#include <functional>
 
-#define BIA_ASSERT(expr, msg) if(!(expr))throw msg;
 
-
-template<typename T>
-inline bool TestValue(bia::machine::BiaMachineContext & p_context, const std::string & p_stVariable, T p_value)
+class test
 {
-	//Get variable
-	auto pVariable = p_context.GetGlobal(p_stVariable);
+public:
+	static void add_test(const char * _name, std::function<void()> && _test)
+	{
+		_tests[_name] = std::move(_test);
+	}
+	template<typename Type>
+	static void assert_equals(Type _actual, Type _expected, const char * _message...)
+	{
+		va_list _args;
+		va_start(_args, _message);
 
-	BIA_ASSERT(pVariable != nullptr, "Variable not found.");
+		assert(_actual == _expected, _message, _args);
 
-	//Get value
-	auto pValue = pVariable->Cast<T>();
+		va_end(_args);
+	}
+	static void assert_true(bool _condition, const char * _message...)
+	{
+		va_list _args;
+		va_start(_args, _message);
 
-	BIA_ASSERT(pValue != nullptr, "Invalid type.");
-	BIA_ASSERT(*pValue == p_value, "Values don't match.");
+		assert(_condition, _message, _args);
 
-	return true;
-}
+		va_end(_args);
+	}
+	static void assert_false(bool _condition, const char * _message...)
+	{
+		va_list _args;
+		va_start(_args, _message);
 
-template<typename T>
-inline bool TestValue(bia::framework::BiaMember & p_member, T p_value)
-{
-	//Get value
-	auto pValue = p_member.Cast<T>();
+		assert(!_condition, _message, _args);
 
-	BIA_ASSERT(pValue != nullptr, "Invalid type.");
-	BIA_ASSERT(*pValue == p_value, "Values don't match.");
+		va_end(_args);
+	}
+	static void fail(const char * _message...)
+	{
 
-	return true;
-}
+	}
+	static bool test_main(const char * _mode)
+	{
+		try {
+			// Test all
+			if (!_mode) {
+				for (auto & _test : _tests) {
+					try {
+						_test.second();
+					} catch (...) {
+						_mode = _test.first;
+					}
+				}
+			} else {
+				auto _test = _tests.find(_mode);
 
-inline bool TestValue(bia::framework::BiaMember & p_member, const char * p_szValue)
-{
-	//Get value
-	auto pValue = p_member.Cast<const char*>();
+				// Test not found
+				if (_test == _tests.end()) {
+					error("Test %s not found", _mode);
 
-	BIA_ASSERT(pValue != nullptr, "Invalid type.");
-	BIA_ASSERT(std::strcmp(*pValue, p_szValue) == 0, "Values don't match.");
+					return false;
+				} else {
+					_test->second();
+				}
+			}
 
-	return true;
-}
+			return true;
+		} catch (const std::exception & e) {
+			error("%s threw %s: %s", _mode, typeid(e).name(), e.what());
+		} catch (...) {
+			error("%s threw an exception", _mode);
+		}
+
+		return false;
+	}
+
+private:
+	struct cstring_compare
+	{
+		constexpr bool operator()(const char * _left, const char * _right) const
+		{
+			return std::strcmp(_left, _right) < 0;
+		}
+	};
+
+	typedef std::map<const char*, std::function<void()>, cstring_compare> tests_type;
+
+	static tests_type _tests;
+
+	static void assert(bool _condition, const char * _message, va_list _args)
+	{
+		if (!_condition) {
+			throw;
+		}
+	}
+	static void error(const char * _message...)
+	{
+
+	}
+};
