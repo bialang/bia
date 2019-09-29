5d5be1af3d73616e86623e1ecfbc61eebae0f90a
Yunus Ayar <greybley@yahoo.de>
2019-09-29 13:08:48 +0200

    Update buffer_output_stream

--- bia/stream/src/buffer_output_stream.cpp
+++ bia/stream/src/buffer_output_stream.cpp
@@ -1,9 +1,35 @@
-#include <gc/gc.hpp>
-#include <stream/buffer_output_stream.hpp>
 #include <cstring>
+#include <stream/buffer_output_stream.hpp>
 
 namespace bia {
 namespace stream {
+
+bia::stream::buffer_output_stream::buffer_output_stream(bia::gc::memory_allocator* allocator) noexcept
+{
+	current_size	= 0;
+	max_size		= 0;
+	buffer			= nullptr;
+	this->allocator = allocator;
+}
+
+buffer_output_stream::buffer_output_stream(buffer_output_stream&& move)
+{
+	current_size = move.current_size;
+	max_size	 = move.max_size;
+	buffer		 = move.max_size;
+	allocator	= move.allocator;
+
+	move.current_size = 0;
+	move.max_size	 = 0;
+	move.buffer		  = write_all;
+}
+
+buffer_output_stream::~buffer_output_stream()
+{
+	if (buffer) {
+		allocator->deallocate(buffer, 0);
+	}
+}
 
 void buffer_output_stream::write(const void* buffer, std::size_t size)
 {
@@ -16,6 +42,26 @@ void buffer_output_stream::write(const void* buffer, std::size_t size)
 
 void buffer_output_stream::reserve(std::size_t additional_size)
 {
+	const auto end = current_size + additional_size;
+	auto new_max   = max_size;
+
+	while (end > new_max) {
+		new_max *= 2;
+	}
+
+	if (new_max != max_size) {
+		auto new_buffer = allocator->allocate(new_max, 0);
+
+		// copy & deallocate
+		if (buffer) {
+			std::memcpy(new_buffer, buffer, current_size);
+
+			allocator->deallocate(buffer, 0);
+		}
+
+		buffer   = new_buffer;
+		max_size = new_max;
+	}
 }
 
 const void* buffer_output_stream::data() const noexcept
--- bia/stream/stream/buffer_output_stream.hpp
+++ bia/stream/stream/buffer_output_stream.hpp
@@ -3,6 +3,7 @@
 #include "output_stream.hpp"
 
 #include <cstdint>
+#include <gc/memory_allocator.hpp>
 
 namespace bia {
 namespace stream {
@@ -10,6 +11,10 @@ namespace stream {
 class buffer_output_stream : public output_stream
 {
 public:
+	buffer_output_stream(bia::gc::memory_allocator* allocator) noexcept;
+	buffer_output_stream(const buffer_output_stream& copy) = delete;
+	buffer_output_stream(buffer_output_stream&& move);
+	virtual ~buffer_output_stream();
 	virtual void write(const void* buffer, std::size_t size) override;
 	virtual void reserve(std::size_t additional_size) override;
 	const void* data() const noexcept;
@@ -19,6 +24,7 @@ private:
 	std::size_t current_size;
 	std::size_t max_size;
 	std::int8_t* buffer;
+	gc::memory_allocator* allocator
 };
 
 }
