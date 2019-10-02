6d4c6b17a351c77cecdf92830ca13cb15df4bbe9
Yunus Ayar <greybley@yahoo.de>
2019-10-02 11:27:17 +0200

    Add performance test

---
+++ bia/string/tests/unicode_performance.cpp
@@ -0,0 +1,43 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+#include <catch.hpp>
+
+#include <string/encoding/standard/unicode.hpp>
+
+using namespace bia::string::encoding::standard;
+
+TEST_CASE("standard unicode", "[string][encoding][standrad]")
+{
+	BENCHMARK("category of: a")
+	{
+		return category_of('a');
+	};
+
+	BENCHMARK("category of: 0x1a4a")
+	{
+		return category_of(0x1a4a);
+	};
+
+	BENCHMARK("category of: 0x257e")
+	{
+		return category_of(0x257e);
+	};
+
+	BENCHMARK("category of: 0x10850")
+	{
+		return category_of(0x10850);
+	};
+
+	BENCHMARK("category of: 0x1d697")
+	{
+		return category_of(0x1d697);
+	};
+
+	BENCHMARK("category of: 0x10ffff")
+	{
+		return category_of(0x10ffff);
+	};
+
+	REQUIRE(category_of('a') == CATEGORY::Ll);
+	REQUIRE(category_of('A') == CATEGORY::Lu);
+}
