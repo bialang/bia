48ee4d398d6aa10923986796f9c1a02d2e90e091
Yunus Ayar <greybley@yahoo.de>
2019-10-02 11:29:39 +0200

    Add standard unicode category of

--- bia/string/string/encoding/standard/unicode.hpp
+++ bia/string/string/encoding/standard/unicode.hpp
@@ -2,20 +2,71 @@
 
 #include "../encoder.hpp"
 
+#include <cstdint>
+
 namespace bia {
 namespace string {
 namespace encoding {
 namespace standard {
 
-inline bool is_valid_unicode(code_point value)
+enum class CATEGORY : std::int8_t
 {
-    return value <= 0x10ffff &&
-        value < 0xd800 && value > 0xdfff &&
-        value < 0xfdd0 && value > 0xfdef &&
-        (value & 0xfffe) != 0xfffe;
+	UNASSIGNED,
+	Cc,
+	Cf,
+	Co,
+	Cs,
+	Ll,
+	Lm,
+	Lo,
+	Lt,
+	Lu,
+	Mc,
+	Me,
+	Mn,
+	Nd,
+	Nl,
+	No,
+	Pc,
+	Pd,
+	Pe,
+	Pf,
+	Pi,
+	Po,
+	Ps,
+	Sc,
+	Sk,
+	Sm,
+	So,
+	Zl,
+	Zp,
+	Zs
+};
+
+CATEGORY category_of(code_point value) noexcept;
+
+inline bool is_letter(code_point value) noexcept
+{
+	auto category = category_of(value);
+
+	return category == CATEGORY::Ll || category == CATEGORY::Lm || category == CATEGORY::Lo ||
+		   category == CATEGORY::Lt || category == CATEGORY::Lu;
 }
 
+inline bool is_number(code_point value) noexcept
+{
+	auto category = category_of(value);
+
+	return category == CATEGORY::Nd || category == CATEGORY::Nl || category == CATEGORY::No;
 }
+
+inline bool is_valid_unicode(code_point value)
+{
+	return value <= 0x10ffff && value < 0xd800 && value > 0xdfff && value < 0xfdd0 && value > 0xfdef &&
+		   (value & 0xfffe) != 0xfffe;
 }
-}
-}
+
+} // namespace standard
+} // namespace encoding
+} // namespace string
+} // namespace bia
