e74145c1e9ee1cf1542914b309f34d98a6dbe038
Yunus Ayar <greybley@yahoo.de>
2019-10-12 23:10:34 +0200

    Update op codes

--- bia/bytecode/bytecode/op_code.hpp
+++ bia/bytecode/bytecode/op_code.hpp
@@ -8,6 +8,19 @@ namespace bytecode {
 typedef int op_code_type;
 constexpr auto max_instruction_size = 2;
 
+struct string_index
+{
+	typedef std::uint64_t index_type;
+
+	const index_type index;
+
+	string_index(index_type index) : index(index)
+	{}
+};
+
+struct test_register
+{};
+
 enum MEMBER_OP_CODE_OPTION
 {
 	MOCO_LOCAL,
@@ -28,9 +41,12 @@ enum IMMEDIATE_INT_OP_CODE_OPTION
 
 enum IMMEADIATE_OP_CODE_OPTION
 {
-	ICOC_INT8,
-	IOCO_INT32,
-	IOCO_INT64,
+	ICOC_INT8,			// a constant 8 bit value
+	IOCO_INT32,			// a constant 32 bit value
+	IOCO_INT64,			// a constant 64 bit value
+	IOCO_FLOAT,			// a constant 64 bit double precision IEEE 754 floating point value
+	IOCO_TEST_REGISTER, // the current value in the test register
+	IOCO_STRING,		// a string
 
 	IOCO_COUNT
 };
@@ -46,12 +62,21 @@ enum OP_CODE : op_code_type
 	OC_JUMP_FALSE =
 		OC_JUMP_TRUE + IIOCO_COUNT, // jumps to a relative location if the test register evaluates to `false`
 
+	/* immediate instruction */
+	OC_PUSH_IMMEDIATE   = OC_JUMP_FALSE + IOCO_COUNT,	 // pushes an immediate value
+	OC_RETURN_IMMEDIATE = OC_PUSH_IMMEDIATE + IOCO_COUNT, // returns an immediate
+
+	/* member instruction */
+	OC_PUSH_MEMBER   = OC_RETURN_IMMEDIATE + MOCO_COUNT,
+	OC_RETURN_MEMBER = OC_PUSH_MEMBER + MOCO_COUNT,
+	OC_TEST			 = OC_RETURN_MEMBER + MOCO_COUNT,
+
 	/* member-immeadiate instruction */
-	OC_INSTANTIATE = MOCO_COUNT * IOCO_COUNT, // instantiates a member with the given immediate
+	OC_INSTANTIATE = OC_TEST + MOCO_COUNT * IOCO_COUNT, // instantiates a member with the given immediate
 
 	/* member-member instructions */
-	OC_CLONE		= MOCO_COUNT * MOCO_COUNT,			  // clones the second member to the first
-	OC_SHALLOW_COPY = OC_CLONE + MOCO_COUNT * MOCO_COUNT, // makes a shallow copy of the second object to the first one
+	OC_SHALLOW_COPY =
+		OC_INSTANTIATE + MOCO_COUNT * MOCO_COUNT, // makes a shallow copy of the second object to the first one
 	OC_DEEP_COPY = OC_SHALLOW_COPY + MOCO_COUNT * MOCO_COUNT, // makes a deep copy of the second object to the first one
 	OC_REFER	 = OC_DEEP_COPY + MOCO_COUNT * MOCO_COUNT,	// makes a references
 };
--- bia/bytecode/bytecode/writer.hpp
+++ bia/bytecode/bytecode/writer.hpp
@@ -17,7 +17,21 @@ inline typename std::enable_if<bia::util::type_traits::equals_any<OP_CODE, Op_co
 }
 
 template<OP_CODE Op_code, typename T>
-inline typename std::enable_if<bia::util::type_traits::equals_any<OP_CODE, Op_code, OC_JUMP, OC_JUMP_TRUE, OC_JUMP_FALSE>::value && bia::util::type_traits::equals_any_type<T, std::int8_t, std::int32_t>::value, void>::type
+inline typename std::enable_if<
+	bia::util::type_traits::equals_any<OP_CODE, Op_code, OC_JUMP, OC_JUMP_TRUE, OC_JUMP_FALSE>::value &&
+		bia::util::type_traits::equals_any_type<T, std::int8_t, std::int32_t>::value,
+	void>::type
+	write_instruction(bia::stream::output_stream& output, T value)
+{
+	output.write_all(Op_code, value);
+}
+
+template<OP_CODE Op_code, typename T>
+inline typename std::enable_if<
+	bia::util::type_traits::equals_any<OP_CODE, Op_code, OC_PUSH_IMMEDIATE, OC_RETURN_IMMEDIATE>::value &&
+		bia::util::type_traits::equals_any_type<T, std::int8_t, std::int32_t, std::int64_t, double,
+												test_register, string_index>::value,
+	void>::type
 	write_instruction(bia::stream::output_stream& output, T value)
 {
 	output.write_all(Op_code, value);
