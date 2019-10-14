5932c1e23dd8aa7a0806981f1fbe75742c4ace61
Yunus Ayar <greybley@yahoo.de>
2019-10-14 11:24:32 +0200

    Update equals_any.hpp

--- bia/util/util/type_traits/equals_any.hpp
+++ bia/util/util/type_traits/equals_any.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <type_traits>
 
 namespace bia {
@@ -7,33 +8,42 @@ namespace util {
 namespace type_traits {
 
 /* equals any */
+template<std::size_t Index, typename T, T Value, T... Others>
+struct equals_any_t;
+
+template<std::size_t Index, typename T, T Value, T Other>
+struct equals_any_t<Index, T, Value, Other>
+	: std::conditional<Value == Other, std::integral_constant<std::size_t, Index>,
+					   std::integral_constant<std::size_t, 0>>::type
+{};
+
+template<std::size_t Index, typename T, T Value, T Other, T... Others>
+struct equals_any_t<Index, T, Value, Other, Others...>
+	: std::conditional<Value == Other, std::integral_constant<std::size_t, Index>,
+					   equals_any_t<Index + 1, T, Value, Others...>>::type
+{};
+
 template<typename T, T Value, T... Other>
-struct equals_any : std::false_type
-{};
-
-template<typename T, T Value, T Other>
-struct equals_any<T, Value, Other> : std::conditional<Value == Other, std::true_type, std::false_type>::type
-{};
-
-template<typename T, T Value, T Other, T... Others>
-struct equals_any<T, Value, Other, Others...>
-	: std::conditional<Value == Other, std::true_type, equals_any<T, Value, Others...>>::type
-{};
+using equals_any = equals_any_t<1, T, Value, Other...>;
 
 /* equals any type */
-template<typename T, typename... Other>
-struct equals_any_type : std::false_type
+template<std::size_t Index, typename T, typename... Others>
+struct equals_any_type_t;
+
+template<std::size_t Index, typename T, typename Other>
+struct equals_any_type_t<Index, T, Other>
+	: std::conditional<std::is_same<T, Other>::value, std::integral_constant<std::size_t, Index>,
+					   std::integral_constant<std::size_t, 0>>::type
 {};
 
-template<typename T, typename Other>
-struct equals_any_type<T, Other>
-	: std::conditional<std::is_same<T, Other>::value, std::true_type, std::false_type>::type
+template<std::size_t Index, typename T, typename Other, typename... Others>
+struct equals_any_type_t<Index, T, Other, Others...>
+	: std::conditional<std::is_same<T, Other>::value, std::integral_constant<std::size_t, Index>,
+					   equals_any_type_t<Index + 1, T, Others...>>::type
 {};
 
-template<typename T, typename Other, typename... Others>
-struct equals_any_type<T, Other, Others...>
-	: std::conditional<std::is_same<T, Other>::value, std::true_type, equals_any_type<T, Others...>>::type
-{};
+template<typename T, typename... Others>
+using equals_any_type = equals_any_type_t<1, T, Others...>;
 
 } // namespace type_traits
 } // namespace util
