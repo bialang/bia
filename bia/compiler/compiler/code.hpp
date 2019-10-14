767b1213cd80e6fa0689df68c3ab9e509c15c78e
Yunus Ayar <greybley@yahoo.de>
2019-10-14 11:25:00 +0200

    Update code.hpp

--- bia/compiler/compiler/code.hpp
+++ bia/compiler/compiler/code.hpp
@@ -8,22 +8,39 @@ namespace compiler {
 class code
 {
 public:
+	code(const void* b, const void* e) : b(b), e(e)
+	{}
 	code(const code& copy) = delete;
-	const void* begin() const noexcept;
-	const void* end() const noexcept;
+	const void* begin() const noexcept
+	{
+		return b;
+	}
+	const void* end() const noexcept
+	{
+		return e;
+	}
 	/*
 	 Returns the count of required local members.
 
 	 @returns the count
 	*/
-	std::size_t local_member_count() const;
+	std::size_t local_member_count() const
+	{
+		return 0;
+	}
 	/*
 	 Returns the count of required temporary members.
 
 	 @returns the count
 	*/
-	std::size_t temp_member_count() const;
+	std::size_t temp_member_count() const
+	{
+		return 0;
+	}
 
+private:
+	const void* b;
+	const void* e;
 };
 
 } // namespace compiler
