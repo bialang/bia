1a0b7d3c9c5ffc94a8b5f4d028e7fc2b2064c8fe
Yunus Ayar <greybley@yahoo.de>
2020-01-30 15:29:28 +0100

    Update memory allocator

--- bia/gc/gc/memory_allocator.hpp
+++ bia/gc/gc/memory_allocator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <util/gsl.hpp>
 
 namespace bia {
 namespace gc {
@@ -15,42 +16,37 @@ class memory_allocator
 public:
 	/* the alignment of the allocated memory */
 	constexpr static std::size_t alignment = alignof(std::max_align_t);
-	/* the alignment of the previous data */
-	constexpr static std::size_t previous_data_alignment = 1;
 
 	/*
 	 Destructor.
 	*/
 	virtual ~memory_allocator() = default;
-
 	/*
 	 Deallocates the memory prior allocated by allocate(). This function is thread-safe to call.
 
-	 @param ptr is the memory that should be freed
+	 @param ptr is the memory that should be freed; if equal to `nullptr` this operation is a noop
 	 @param previous_size must be the same as during allocation, otherwise undefined behavior
 	*/
-	virtual void deallocate(void* ptr, std::size_t previous_size) = 0;
+	virtual void deallocate(void* ptr) = 0;
 	/*
 	 Allocates *size* bytes and returns the pointer to the first byte. This function is thread-safe to call. The memory
 	 is aligned according to memory_allocator::alignment.
 
 	 @param size is the amount of required bytes
-	 @param previous_size is the amount of bytes required before the first byte; this memory has an alignment of 1
 	 @returns the pointer to the first byte if successful, otherwise `nullptr`
 	*/
-	virtual void* allocate(std::size_t size, std::size_t previous_size) = 0;
+	virtual void* allocate(std::size_t size) = 0;
 	/*
 	 Calls allocate() and returns the non-null result. If allocate() returns `nullptr`, this function will throw a ...
 	 
 	 @param size is the amount of required bytes
-	 @param previous_size is the amount of bytes required before the first byte; this memory has an alignment of 1
 	 @returns the pointer to the first byte
 	 @throws ... if the allocation failed
 	 @todo add exception
 	*/
-	void* checked_allocate(std::size_t size, std::size_t previous_size)
+	util::not_null<void*> checked_allocate(std::size_t size)
 	{
-		auto ptr = allocate(size, previous_size);
+		auto ptr = allocate(size);
 
 		if (!ptr) {
 		}
@@ -62,24 +58,10 @@ public:
 
 	 @returns `true` if *x* is a power of 2, otherwise `false`
 	*/
-	static bool power_of_two(std::size_t x) noexcept
+	constexpr static bool power_of_two(std::size_t x) noexcept
 	{
 		return !x && !(x & (x - 1));
 	}
-	/*
-	 Sets the default memory allocator. This allocator must be created by `new` and will be automatically deleted by
-	 `delete`. This function is not thread-safe and it is dangerous to change the allocator after a default allocator
-	 was already acquired.
-
-	 @param[in] allocator is the new default allocator
-	*/
-	static void default_allocator(memory_allocator* allocator) noexcept;
-	/*
-	 Returns the default memory allocator.
-
-	 @returns the default memory allocator
-	*/
-	static memory_allocator* default_allocator() noexcept;
 };
 
 } // namespace gc
