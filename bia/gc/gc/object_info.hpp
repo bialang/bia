c2b3c33f9269165f26fd217f2eba2c9670b8d63d
Yunus Ayar <greybley@yahoo.de>
2020-01-30 19:35:20 +0100

    Update object_info.hpp

--- bia/gc/gc/object_info.hpp
+++ bia/gc/gc/object_info.hpp
@@ -13,9 +13,12 @@ struct alignas(object_alignment) object_info
 	/** an index for the garbage collector indicating if this value was missed */
 	std::atomic_uint32_t miss_index;
 	/** the current mark */
-	std::atomic_bool mark;
+	bool mark;
 	/** whether this object is a leaf or a node */
 	const bool leaf;
+
+	object_info(bool mark, bool leaf) noexcept : mark(mark), leaf(leaf)
+	{}
 };
 
 } // namespace gc
