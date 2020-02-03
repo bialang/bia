a62e3a1ac33951968d9d3665fd1bc3ff88a49b28
Yunus Ayar <greybley@yahoo.de>
2020-02-03 19:42:36 +0100

    Create token.hpp

---
+++ bia/tokenizer/tokenizer/token.hpp
@@ -0,0 +1,13 @@
+#ifndef BIA_TOKENIZER_TOKEN_HPP_
+#define BIA_TOKENIZER_TOKEN_HPP_
+
+namespace bia {
+namespace tokenizer {
+
+struct token
+{};
+
+} // namespace tokenizer
+} // namespace bia
+
+#endif
