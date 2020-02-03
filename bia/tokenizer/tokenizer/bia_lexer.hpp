8538f8ba16f86f9ef03266206afd663818643aab
Yunus Ayar <greybley@yahoo.de>
2020-02-03 20:44:22 +0100

    Create bia_lexer.hpp

---
+++ bia/tokenizer/tokenizer/bia_lexer.hpp
@@ -0,0 +1,20 @@
+#ifndef BIA_TOKENIZER_BIA_LEXER_HPP_
+#define BIA_TOKENIZER_BIA_LEXER_HPP_
+
+#include "lexer.hpp"
+
+namespace bia {
+namespace tokenizer {
+
+class bia_lexer : public lexer
+{
+public:
+	virtual void lex(std::istream& input, string::encoding::encoder& encoder,
+	                 token_receiver& receiver) const override;
+private:
+};
+
+} // namespace tokenizer
+} // namespace bia
+
+#endif
