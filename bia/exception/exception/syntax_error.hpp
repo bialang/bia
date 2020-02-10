1a75ea2b643ae9e608d7cbd997d318fe90bfb605
Yunus Ayar <greybley@yahoo.de>
2020-02-10 19:50:56 +0100

    Update syntax_error.hpp

--- bia/exception/exception/syntax_error.hpp
+++ bia/exception/exception/syntax_error.hpp
@@ -10,8 +10,29 @@ namespace exception {
 
 struct syntax_details
 {
+	/** whether these error details are valid */
 	bool valid;
-	std::istream::pos_type position = -1;
+	/** the position in the stream where the error occurred */
+	std::istream::pos_type position;
+	/** a static error message */
+	const char* message;
+
+	syntax_details() noexcept
+	{
+		valid    = false;
+		position = -1;
+		message  = nullptr;
+	}
+	syntax_details(std::istream::pos_type position, const char* message) noexcept
+	{
+		valid          = true;
+		this->position = position;
+		this->message  = message;
+	}
+	operator bool() const noexcept
+	{
+		return valid;
+	}
 };
 
 class syntax_error : public bia_error
