bac6b8ecce3fa498c346367567be943e1dd4e542
Yunus Ayar <greybley@yahoo.de>
2020-02-11 21:16:18 +0100

    Update whitespace eater

--- bia/tokenizer/tokenizer/token/whitespace_eater.hpp
+++ bia/tokenizer/tokenizer/token/whitespace_eater.hpp
@@ -1,15 +1,43 @@
 #ifndef BIA_TOKENIZER_TOKEN_WHITESPACE_EATER_HPP_
 #define BIA_TOKENIZER_TOKEN_WHITESPACE_EATER_HPP_
 
+#include "token_parameter.hpp"
+
 #include <exception/syntax_error.hpp>
-#include <istream>
-#include <string/encoding/encoder.hpp>
 
 namespace bia {
 namespace tokenizer {
 namespace token {
 
-exception::syntax_details eat_whitespaces(std::istream& input, string::encoding::encoder& encoder);
+inline exception::syntax_details eat_whitespaces(token_parameter& token_parameter)
+{
+	auto eaten = false;
+
+	while (true) {
+		auto pos = token_parameter.input.tellg();
+		auto cp  = token_parameter.encoder.read(token_paramter.input);
+
+		switch (cp) {
+		case ' ':
+		case '\t': {
+			eaten = true;
+
+			break;
+		}
+		default: {
+			input.seekg(pos);
+
+			// expected whitespaces
+			if (!eaten) {
+				return { pos, "expected whitespaces" };
+			}
+
+			// no error
+			return {};
+		}
+		}
+	}
+}
 
 } // namespace token
 } // namespace tokenizer
