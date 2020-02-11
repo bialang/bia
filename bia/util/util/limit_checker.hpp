26ca2895b2ad861d9bd720af9822fa5d86054377
Yunus Ayar <greybley@yahoo.de>
2020-02-11 19:33:13 +0100

    Update limit_checker.hpp

--- bia/util/util/limit_checker.hpp
+++ bia/util/util/limit_checker.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef BIA_UTIL_LIMIT_CHECKER_HPP_
+#define BIA_UTIL_LIMIT_CHECKER_HPP_
 
 #include <limits>
 #include <type_traits>
@@ -13,7 +14,7 @@ template<typename T>
 struct limit_checker<T, typename std::enable_if<std::is_unsigned<T>::value>::type>
 {
 	template<typename V>
-	static bool check(V&& value)
+	constexpr static bool in_bounds(V value) noexcept
 	{
 		return value <= std::numeric_limits<T>::max();
 	}
@@ -23,7 +24,7 @@ template<typename T>
 struct limit_checker<T, typename std::enable_if<std::is_signed<T>::value>::type>
 {
 	template<typename V>
-	static bool check(V&& value)
+	constexpr static bool in_bounds(V value) noexcept
 	{
 		return value >= std::numeric_limits<T>::min() && value <= std::numeric_limits<T>::max();
 	}
@@ -31,3 +32,5 @@ struct limit_checker<T, typename std::enable_if<std::is_signed<T>::value>::type>
 
 } // namespace util
 } // namespace bia
+
+#endif
