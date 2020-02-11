a44b534b48f47b2171da67e1ed0c1b522aedc5a9
Yunus Ayar <greybley@yahoo.de>
2020-02-11 21:28:10 +0100

    Update statement

--- bia/tokenizer/tokenizer/token/statement.hpp
+++ bia/tokenizer/tokenizer/token/statement.hpp
@@ -1,21 +1,44 @@
 #ifndef BIA_TOKENIZER_TOKEN_STATEMENT_HPP_
 #define BIA_TOKENIZER_TOKEN_STATEMENT_HPP_
 
-#include "../resource_manager.hpp"
+#include "token_parameter.hpp"
 
 #include <exception/syntax_error.hpp>
-#include <istream>
-#include <string/encoding/encoder.hpp>
 
 namespace bia {
 namespace tokenizer {
 namespace token {
 
-exception::syntax_details single_stmt(std::istream& input, string::encoding::encoder& encoder,
-                                      resource_manager& resource_manager);
+exception::syntax_details decl_stmt(token_parameter& token_parameter)
+{
+	// compare let
+	if (auto err = keyword("let", token_parameter)) {
+		return err;
+	}
 
-exception::syntax_details decl_stmt(std::istream& input, string::encoding::encoder& encoder,
-                                    resource_manager& resource_manager);
+	// whitespaces are required
+	if (auto err = eat_whitespaces(token_parameter)) {
+		return err;
+	}
+
+	identifier(token_parameter);
+
+	// optional whitespaces
+	eat_whitespaces(token_parameter);
+    
+	// compare '='
+	auto pos = token_parameter.input.tellg();
+
+	if (token_parameter.encoder.read(token_parameter.input) != '=') {
+		return { pos, "expected assignment operator" };
+	}
+
+	// optional whitespaces
+	eat_whitespaces(token_parameter);
+
+	// read expression
+	return expression(token_parameter);
+}
 
 } // namespace token
 } // namespace tokenizer
