1746ad3f79cf8ff27bfb70ee839552cf4a603c19
Yunus Ayar <greybley@yahoo.de>
2020-02-11 21:14:33 +0100

    Update identifier

--- bia/tokenizer/tokenizer/token/identifier.hpp
+++ bia/tokenizer/tokenizer/token/identifier.hpp
@@ -1,17 +1,65 @@
 #ifndef BIA_TOKENZIER_TOKEN_IDENTIFIER_HPP_
 #define BIA_TOKENZIER_TOKEN_IDENTIFIER_HPP_
 
-#include "../resource_manager.hpp"
+#include "token_parameter.hpp"
 
 #include <exception/syntax_error.hpp>
-#include <istream>
-#include <string/encoding/encoder.hpp>
+#include <util/finally.hpp>
 
 namespace bia {
 namespace tokenizer {
 namespace token {
 
-exception::syntax_details identifier(std::istream& input, string::encoding::encoder& encoder, resource_manager& resource_manager);
+inline exception::syntax_details identifier(token_parameter& token_parameter)
+{
+	using namespace string::encoding;
+
+	auto first     = true;
+	auto streambuf = token_parameter.resource_manager.start_memory(true);
+	auto outenc    = encoder::get_instance(encoder::standard_encoding::utf_8);
+	auto free      = util::make_finally([outenc] { encoder::free_instance(outenc); });
+
+	std::ostream output(&streambuf);
+
+	while (true) {
+		auto pos = token_parameter.input.tellg();
+		auto cp  = token_parameter.encoder.read(token_parameter.input);
+
+		switch (category_of(cp)) {
+		case category::Ll:
+		case category::Lu:
+		case category::Lt:
+		case category::Lo:
+		case category::Pc:
+		case category::Nl: first = false;
+		case category::Nd:
+		case category::No: {
+			// add to output
+			if (!first) {
+				outenc->put(output, cp);
+
+				break;
+			}
+		}
+		default: {
+			// invalid char -> reset
+			input.seekg(pos);
+
+			// valid identifier
+			if (!first) {
+				// zero terminate
+				outenc->put(output, 0);
+				streambuf.commit_close();
+
+				return {};
+			}
+
+			// not an identifier
+			return { pos, "invalid identifier" };
+		}
+		}
+	}
+}
 
 } // namespace token
 } // namespace tokenizer
