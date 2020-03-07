36683eb00497434d052e0a7d4d5fd5951a671126
terrakuh <greybley@yahoo.de>
2020-03-07 14:20:28 +0100

    Use streambuf instead of iterator

--- bia/tokenizer/tokenizer/resource_manager.hpp
+++ bia/tokenizer/tokenizer/resource_manager.hpp
@@ -6,6 +6,7 @@
 #include <gc/std_memory_allocator.hpp>
 #include <iterator>
 #include <map>
+#include <streambuf>
 #include <util/gsl.hpp>
 #include <vector>
 
@@ -21,11 +22,17 @@ public:
 		state()                  = default;
 		state(const state& copy) = default;
 		state(state&& move)      = default;
-		std::size_t page_index();
-		util::byte* cursor();
+		std::size_t page_index() noexcept
+		{
+			return _page_index;
+		}
+		util::byte* cursor() noexcept
+		{
+			return _cursor;
+		}
 
 	private:
-		friend resource_manager;
+		friend class resource_manager;
 
 		std::size_t _page_index = 0;
 		util::byte* _cursor     = nullptr;
@@ -37,107 +44,60 @@ public:
 		std::uint16_t size : 15;
 	};
 
-	class output_iterator
+	class output_streambuf : public std::streambuf
 	{
 	public:
-		typedef std::output_iterator_tag iterator_category;
-		typedef util::byte value_type;
-		typedef std::ptrdiff_t difference_type;
-		typedef value_type* pointer;
-		typedef value_type& reference;
-
-		/**
-		 * Advances the output iterator by one byte.
-		 *
-		 * @pre `valid() == true`
-		 *
-		 * @returns a reference to this object
-		 * @throw see resource_manager::_next_page()
-		 */
-		output_iterator& operator++()
+	protected:
+		int_type sync() override
 		{
-			BIA_EXPECTS(valid());
+			// get next page
+			if (gptr() == egptr()) {
+				if (_size) {
+					_size->more = 1;
+				}
 
-			_size->size++;
+				try {
+					auto page = _resource_manager->_next_page();
 
-			// reached page end
-			if (++_cursor >= _page_end) {
-				// allocate new page
-				std::tie(_cursor, _page_end) = _resource_manager->_next_page();
+					_size = new (page.first) size{};
 
-				// update size poiter
-				_size->more = 1;
-				_size       = reinterpret_cast<size*>(_cursor);
-
-				_cursor += sizeof(size);
+					setp(reinterpret_cast<char*>(_size + 1), reinterpret_cast<char*>(page.second));
+				} catch (const exception::bia_error&) {
+					return -1;
+				}
 			}
 
-			return *this;
+			return 0;
 		}
-		output_iterator operator++(int)
+		int_type overflow(int_type c) override
 		{
-			output_iterator tmp(*this);
+			if (sync() == 0) {
+				if (c != traits_type::eof()) {
+					sputc(traits_type::to_char_type(c));
 
-			operator++();
+					return c;
+				}
 
-			return tmp;
-		}
-		/**
-		 * Dereferences the buffer at the current position.
-		 *
-		 * @pre `valid() == true`
-		 *
-		 * @returns a reference to the current byte
-		 */
-		reference operator*()
-		{
-			BIA_EXPECTS(valid());
-
-			return *_cursor;
-		}
-		void commit_close()
-		{
-			BIA_EXPECTS(valid());
-
-			// align cursor to struct size
-			if (reinterpret_cast<std::intptr_t>(_cursor) % alignof(size)) {
-				_cursor += alignof(size) - reinterpret_cast<std::intptr_t>(_cursor) % alignof(size);
+				return c;
 			}
 
-			// update state and close
-			_resource_manager->_state.cursor = _cursor;
-			_resource_manager                = nullptr;
-		}
-		/**
-		 * Checks whether this iterator is valid.
-		 *
-		 * @returns `true` if valid, otherwise `false`
-		 */
-		bool valid() const noexcept
-		{
-			return _resource_manager != nullptr;
+			return traits_type::eof();
 		}
 
 	private:
 		friend class resource_manager;
 
-		/** the parent resource manager */
-		class resource_manager* _resource_manager;
-		/** the original resource manager state in case of a rollback */
-		state _original_state;
-		/** a pointer to the size location for this memory part; this pointer is updated when the page is
-		 * changed */
+		resource_manager* _resource_manager;
 		size* _size;
-		/** the current byte location */
-		util::byte* _cursor;
-		/** the end of the current page */
-		util::byte* _page_end;
 
-		output_iterator(resource_manager* resource_manager) noexcept
-		    : _original_state(resource_manager->_state)
+		output_streambuf(util::not_null<resource_manager*> resource_manager)
 		{
-			_resource_manager = resource_manager;
-			_size             = reinterpret_cast<size*>(_resource_manager);
+			_resource_manager = resource_manager.get();
+			_size             = nullptr;
+
+			if (sync() != 0) {
+				BIA_THROW(exception::memory_error, "cannot create page");
+			}
 		}
 	};
 
@@ -145,36 +105,40 @@ public:
 	    : _pages(gc::std_memory_allocator<util::byte*>(allocator)), _allocator(std::move(allocator.get()))
 	{
 		// allocate first page
-		_page_size = page_size;
+		_page_size     = page_size;
+		_state._cursor = static_cast<util::byte*>(_allocator->checked_allocate(_page_size).get());
 
-		_pages.push_back(_state.cursor =
-		                     static_cast<util::byte*>(_allocator->checked_allocate(_page_size).get()));
+		_pages.push_back(_state._cursor);
+	}
+	~resource_manager()
+	{
+		for (auto& page : _pages) {
+			_allocator->deallocate(page);
+		}
 	}
 	output_streambuf start_memory(bool avoid_duplicates)
-	{}
+	{
+		return { this };
+	}
 	state save_state() const noexcept
 	{
 		return _state;
 	}
+	void restore_state(const state& old)
+	{}
 
 private:
-	struct compare
-	{
-		bool operator()(const std::uint8_t* left, const std::uint8_t* right) const noexcept
-		{}
-	};
-
 	/** size of a single page */
 	std::size_t _page_size;
 	/** all currently allocated pages */
 	std::vector<util::byte*, gc::std_memory_allocator<util::byte*>> _pages;
 	/** the current state */
-	state _state{};
+	state _state;
 	/** the memory allocator */
 	std::shared_ptr<gc::memory_allocator> _allocator;
 
 	/**
-	 * Allocates the next page if no more pages are available and returns the beginning and the end.
+	 * Allocates the next page if no more pages are available and returns its beginning and the end.
 	 *
 	 * @returns a pair with the beginning and the end for that page
 	 * @throw see gc::memory_allocator::checked_allocate()
@@ -182,15 +146,15 @@ private:
 	std::pair<util::byte*, util::byte*> _next_page()
 	{
 		// no more pages available
-		if (_state.page_index + 1 >= _pages.size()) {
+		if (_state._page_index + 1 >= _pages.size()) {
 			_pages.push_back(static_cast<util::byte*>(_allocator->checked_allocate(_page_size).get()));
 		}
 
-		_state.page_index++;
+		_state._page_index++;
 
-		_state.cursor = _pages[_state.page_index];
+		_state._cursor = _pages[_state._page_index];
 
-		return { _state.cursor, _state.cursor + _page_size };
+		return { _state._cursor, _state._cursor + _page_size };
 	}
 };
 
