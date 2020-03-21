f9dfe9fd70acd443dc9aa399f2409cc17ed41917
terrakuh <greybley@yahoo.de>
2020-03-21 23:37:12 +0100

    Add span to gsl

--- bia/util/util/gsl.hpp
+++ bia/util/util/gsl.hpp
@@ -3,7 +3,9 @@
 
 #include "contract.hpp"
 
+#include <cstddef>
 #include <cstdint>
+#include <type_traits>
 #include <utility>
 
 namespace bia {
@@ -50,6 +52,75 @@ using owner = T;
 
 enum class byte : std::uint8_t
 {
+};
+
+template<typename T>
+class span
+{
+public:
+	typedef T element_type;
+	typedef typename std::remove_cv<T>::type value_type;
+	typedef std::size_t size_type;
+	typedef std::ptrdiff_t difference_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef pointer iterator;
+
+	span() noexcept
+	{
+		_data = nullptr;
+		_size = 0;
+	}
+	span(pointer begin, size_type count)
+	{
+		_data = begin;
+		_size = count;
+	}
+	span(pointer begin, pointer end)
+	{
+		_data = begin;
+		_size = static_cast<size_type>(end - begin);
+	}
+	span(const span& copy) = default;
+	bool empty() const noexcept
+	{
+		return _size;
+	}
+	size_type size() const noexcept
+	{
+		return _size;
+	}
+	size_type size_bytes() const noexcept
+	{
+		return _size * sizeof(value_type);
+	}
+	iterator begin()
+	{
+		return _data;
+	}
+	iterator end()
+	{
+		return _data + _size;
+	}
+	span subspan(std::size_t offset, std::size_t count)
+	{
+		BIA_EX
+
+		return { _data + offset, _data + offset + count };
+	}
+	pointer data() const noexcept
+	{
+		return _data;
+	}
+	reference operator[](size_type index) const
+	{
+		return _data[index];
+	}
+	span& operator=(const span& copy) = default;
+
+private:
+	pointer _data;
+	size_type _size;
 };
 
 } // namespace util
