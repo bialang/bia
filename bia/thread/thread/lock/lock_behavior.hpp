4ffd07242bf2a4dca4338c2d135ccc29c5de823f
terrakuh <greybley@yahoo.de>
2020-03-24 12:17:08 +0100

    Update lock_behavior

--- bia/thread/thread/lock/lock_behavior.hpp
+++ bia/thread/thread/lock/lock_behavior.hpp
@@ -1,19 +1,20 @@
-#ifndef BIA_THREAD_LOCK_BEHAVIOR_HPP_
-#define BIA_THREAD_LOCK_BEHAVIOR_HPP_
+#ifndef BIA_THREAD_LOCK_LOCK_BEHAVIOR_HPP_
+#define BIA_THREAD_LOCK_LOCK_BEHAVIOR_HPP_
 
-#include "config.hpp"
+#include "../config.hpp"
 
-#if defined(BIA_THREAD_BACKEND_STD)
+#if BIA_THREAD_BACKEND_STD
 #	include <mutex>
 #endif
 
 namespace bia {
 namespace thread {
+namespace lock {
 
-#if defined(BIA_THREAD_BACKEND_STD)
+#if BIA_THREAD_BACKEND_STD
 using try_to_lock_tag      = std::try_to_lock_t;
 constexpr auto try_to_lock = std::try_to_lock;
-#elif defined(BIA_THREAD_BACKEND_NONE)
+#elif BIA_THREAD_BACKEND_NONE
 struct try_to_lock_tag
 {
 	explicit try_to_lock_tag() = default;
@@ -22,6 +23,7 @@ struct try_to_lock_tag
 constexpr try_to_lock_tag try_to_lock{};
 #endif
 
+} // namespace lock
 } // namespace thread
 } // namespace bia
 
