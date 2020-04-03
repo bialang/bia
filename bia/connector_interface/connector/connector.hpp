cf719c7989b0e38a6b23408cf6922721d42413b9
terrakuh <greybley@yahoo.de>
2020-04-03 19:53:34 +0200

    Update connector_interface

--- bia/connector_interface/connector/connector.hpp
+++ bia/connector_interface/connector/connector.hpp
@@ -1,23 +1,13 @@
-#pragma once
+#ifndef BIA_CONNECTOR_INTERFACE_CONNECTOR_HPP_
+#define BIA_CONNECTOR_INTERFACE_CONNECTOR_HPP_
 
 #include <member/member.hpp>
-#include <stack/stack.hpp>
 
 namespace bia {
 namespace connector {
 
-typedef member::member::param_count_type param_count_type;
-
-template<typename Return, typename... Args>
-member::member* static_connector(Return (*function)(Args...), stack::stack* stack, param_count_type param_count);
-
-template<typename Return, typename Class, typename... Args>
-member::member* member_connector(Return (Class::*function)(Args...), Class* instance, stack::stack* stack,
-                                 param_count_type param_count);
-
-/*template<typename Return, typename Class, typename... Args>
-member::member* member_connector(Return (Class::*function)(Args...) const, const Class* instance, stack::stack* stack,
-                                 param_count_type param_count);*/
 
 } // namespace connector
 } // namespace bia
+
+#endif
