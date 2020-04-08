3eef0f5c888493240e8531b6163c3bfe19fbcd30
terrakuh <greybley@yahoo.de>
2020-04-08 10:53:30 +0200

    Update bvm.cpp

--- bia/bvm/tests/bvm.cpp
+++ bia/bvm/tests/bvm.cpp
@@ -1,53 +1,27 @@
+#define CATCH_CONFIG_MAIN
+
 #include <bvm/bvm.hpp>
 #include <bytecode/writer.hpp>
-#include <gc/gc.hpp>
-#include <gc/simple_allocator.hpp>
-#include <stream/buffer_output_stream.hpp>
+#include <catch.hpp>
+#include <gc/memory/simple_allocator.hpp>
+#include <sstream>
 
 using namespace bia;
-using namespace bia::bytecode;
+using namespace bia::bvm;
 
-inline void test_foo(long long v)
+TEST_CASE("simple run", "[bvm]")
 {
-	printf("hello %d\n", v);
-}
+	context ctx{ std::make_shared<gc::gc>(std::make_shared<gc::memory::simple_allocator>()) };
+	std::stringstream output;
+	bytecode::instruction_writer iw{ output };
 
-int main(int argc, char** argv)
-{
-	try {
-		gc::gc gc(std::unique_ptr<gc::memory_allocator>(new gc::simple_allocator()), nullptr);
-		stack::stack stack(gc.allocator(), 1024);
+	iw.write_instruction<false, bytecode::oc_instantiate>(bytecode::local_member{ 0 }, 11);
+	iw.write_instruction<false, bytecode::oc_instantiate>(bytecode::local_member{ 0 }, 13);
+	iw.write_instruction<false, bytecode::oc_jump_true>(std::int32_t{-23});
+	iw.write_end();
 
-		bvm::context context(stack, gc);
-		stream::buffer_output_stream output(*gc.allocator());
-		bvm::bvm machine;
-		const void* b;
-		std::size_t s;
+	const auto code = output.str();
 
-		instruction_writer wi(output);
-
-		wi.write_instruction<false, OC_INSTANTIATE>(temp_member_index<std::uint8_t>{ 0 }, 61);
-		wi.write_instruction<false, OC_TEST>(temp_member_index<std::uint8_t>{ 0 });
-		wi.write_end();
-
-		printf("printing code...");
-
-		std::tie(b, s, std::ignore) = output.take_buffer();
-
-		for (std::size_t i = 0; i < s - max_instruction_size - 1; ++i) {
-			if (!(i % 16)) {
-				puts("");
-			}
-
-			printf("%02x ", static_cast<const unsigned char*>(b)[i]);
-		}
-
-		puts("\n(END)\nstarting vm");
-
-		machine.execute(context, compiler::code(b, static_cast<const std::int8_t*>(b) + s));
-	} catch (const exception::throwable& e) {
-		puts(e.debug_message().c_str());
-	} catch (const std::exception& e) {
-		puts(e.what());
-	}
+	bia::bvm::bvm::execute(ctx, reinterpret_cast<const util::byte*>(code.data()),
+	                       reinterpret_cast<const util::byte*>(code.data() + code.length()));
 }
