955a61a97f41327f8d5ad9093184c218ea6acad6
terrakuh <greybley@yahoo.de>
2020-04-08 10:52:23 +0200

    Update connector

---
+++ bia/connector/connector/connector-inl.hpp
@@ -0,0 +1,13 @@
+#ifndef BIA_CONNECTOR_CONNECTOR_INL_HPP_
+#define BIA_CONNECTOR_CONNECTOR_INL_HPP_
+
+#include <connector/connector.hpp>
+
+namespace bia {
+namespace connector {
+	
+
+} // namespace connector
+} // namespace bia
+
+#endif
