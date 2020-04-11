cb92e0204b83852f41c04905d92e263043e25ed1
terrakuh <greybley@yahoo.de>
2020-04-11 18:16:49 +0200

    Update creator.hpp

--- bia/creator/creator/creator.hpp
+++ bia/creator/creator/creator.hpp
@@ -1,8 +1,33 @@
 #ifndef BIA_CREATOR_CREATOR_HPP_
 #define BIA_CREATOR_CREATOR_HPP_
 
+#include <gc/gc.hpp>
+#include <member/native/int_member.hpp>
+#include <type_traits>
+
 namespace bia {
-namespace creator {} // namespace creator
+namespace creator {
+
+template<typename T>
+inline typename std::enable_if<std::is_integral<typename std::decay<T>::type>::value &&
+                                   (sizeof(typename std::decay<T>::type) <= 8),
+                               gc::gcable<member::native::int_member>>::type
+    create(T&& value)
+{
+	return gc::gc::active_gc()->construct<member::native::int_member>(std::forward<T>(value));
+}
+
+template<typename T>
+inline typename std::enable_if<std::is_floating_point<typename std::decay<T>::type>::value &&
+                                   (sizeof(typename std::decay<T>::type) <= 8),
+                               gc::gcable<member::native::int_member>>::type
+    create(T&& value)
+{
+	return {};
+	//return gc::gc::active_gc()->construct<member::native::int_member>(std::forward<T>(value));
+}
+
+} // namespace creator
 } // namespace bia
 
 #endif
