efdd7d795b7b29a27eca99f5653d06b72529b08c
terrakuh <greybley@yahoo.de>
2020-04-11 12:50:52 +0200

    Add common test

--- bia/bytecode/tests/bytecode.cpp
+++ bia/bytecode/tests/bytecode.cpp
@@ -1,10 +1,23 @@
 #define CATCH_CONFIG_MAIN
 
+#include <bytecode/writer/common.hpp>
 #include <bytecode/writer/member.hpp>
 #include <catch.hpp>
+#include <sstream>
+#include <utility>
 
 using namespace bia::bytecode;
 using namespace bia::bytecode::writer;
+
+template<bool Optimize, typename T>
+inline std::size_t optimized_test_write(T&& value)
+{
+	std::stringstream s;
+
+	optimized_write<Optimize>(s, std::forward<T>(value));
+
+	return s.str().length();
+}
 
 TEST_CASE("instruction writer", "[bytecode]")
 {
@@ -20,6 +33,31 @@ TEST_CASE("instruction writer", "[bytecode]")
 	SECTION("immediate ints")
 	{
 	}*/
+}
+
+TEST_CASE("common writer", "[bytecode]")
+{
+	SECTION("unoptimized")
+	{
+		REQUIRE(optimized_test_write<false>(std::int8_t{ 0 }) == 1);
+		REQUIRE(optimized_test_write<false>(std::int16_t{ -128 }) == 2);
+		REQUIRE(optimized_test_write<false>(std::int32_t{ -56 }) == 4);
+		REQUIRE(optimized_test_write<false>(std::int64_t{ 65 }) == 8);
+	}
+
+	SECTION("optimized")
+	{
+		REQUIRE(optimized_test_write<true>(std::int8_t{ 0 }) == 1);
+		REQUIRE(optimized_test_write<true>(std::int16_t{ -128 }) == 1);
+		REQUIRE(optimized_test_write<true>(std::int32_t{ 127 }) == 1);
+		REQUIRE(optimized_test_write<true>(std::int64_t{ -45 }) == 1);
+		REQUIRE(optimized_test_write<true>(std::int16_t{ 8665 }) == 2);
+		REQUIRE(optimized_test_write<true>(std::int32_t{ 5464 }) == 2);
+		REQUIRE(optimized_test_write<true>(std::int64_t{ -9646 }) == 2);
+		REQUIRE(optimized_test_write<true>(std::int32_t{ 546342344 }) == 4);
+		REQUIRE(optimized_test_write<true>(std::int64_t{ -546343344 }) == 4);
+		REQUIRE(optimized_test_write<true>(std::int64_t{ 995656894948946565 }) == 8);
+	}
 }
 
 TEST_CASE("member writer", "[bytecode]")
