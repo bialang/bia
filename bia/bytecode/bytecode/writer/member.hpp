04aed88781713230d5a0692cbec8cc433c549d36
terrakuh <greybley@yahoo.de>
2020-04-11 12:23:58 +0200

    Add member writer

---
+++ bia/bytecode/bytecode/writer/member.hpp
@@ -0,0 +1,66 @@
+#ifndef BIA_BYTECODE_WRITER_MEMBER_HPP_
+#define BIA_BYTECODE_WRITER_MEMBER_HPP_
+
+#include "../op_code.hpp"
+#include "common.hpp"
+
+#include <ostream>
+#include <type_traits>
+#include <util/type_traits/equals_any.hpp>
+#include <util/type_traits/type_index.hpp>
+
+namespace bia {
+namespace bytecode {
+namespace writer {
+
+template<typename T>
+using is_member = util::type_traits::equals_any_type<typename std::decay<T>::type, member::tos, member::args,
+                                                     member::global, member::local, member::resource>;
+template<typename T>
+using is_indexed_member = util::type_traits::equals_any_type<typename std::decay<T>::type, member::args,
+                                                             member::global, member::local, member::resource>;
+template<typename T>
+using is_indexless_member = util::type_traits::equals_any_type<typename std::decay<T>::type, member::tos>;
+template<typename T>
+using is_optimizeable_member =
+    util::type_traits::equals_any_type<typename std::decay<T>::type, member::global, member::local,
+                                       member::resource>;
+template<typename T>
+using is_not_optimizeable_member =
+    util::type_traits::equals_any_type<typename std::decay<T>::type, member::tos, member::args>;
+
+template<bool Optimize, typename T>
+inline typename std::enable_if<is_indexed_member<T>::value>::type optimized_member(std::ostream& output,
+                                                                                   T member)
+{
+	optimized_write<Optimize>(output, member.index);
+}
+
+template<bool Optimize, typename T>
+inline typename std::enable_if<is_indexless_member<T>::value>::type optimized_member(std::ostream&,
+                                                                                     T) noexcept
+{}
+
+template<bool Optimize, typename T>
+constexpr inline typename std::enable_if<is_not_optimizeable_member<T>::value, member_option>::type
+    member_index(T) noexcept
+{
+	return static_cast<member_option>(
+	    util::type_traits::type_index<typename std::decay<T>::type, member::tos, member::args>::value);
+}
+
+template<bool Optimize, typename T>
+constexpr inline typename std::enable_if<is_optimizeable_member<T>::value, member_option>::type
+    member_index(T member) noexcept
+{
+	return static_cast<member_option>(
+	    util::type_traits::type_index<typename std::decay<T>::type, member::global, member::local,
+	                                  member::resource>::value +
+	    mo_global_16 + (Optimize && util::limit_checker<std::uint8_t>::in_bounds(member.index) ? 3 : 0));
+}
+
+} // namespace writer
+} // namespace bytecode
+} // namespace bia
+
+#endif
--- bia/bytecode/tests/bytecode.cpp
+++ bia/bytecode/tests/bytecode.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_MAIN
 
-#include <bytecode/writer.hpp>
+#include <bytecode/writer/member.hpp>
 #include <catch.hpp>
 
-using namespace bia;
 using namespace bia::bytecode;
+using namespace bia::bytecode::writer;
 
 TEST_CASE("instruction writer", "[bytecode]")
 {
@@ -20,4 +20,31 @@ TEST_CASE("instruction writer", "[bytecode]")
 	SECTION("immediate ints")
 	{
 	}*/
+}
+
+TEST_CASE("member writer", "[bytecode]")
+{
+	SECTION("unoptimized")
+	{
+		REQUIRE(member_index<false>(member::tos{}) == mo_tos);
+		REQUIRE(member_index<false>(member::args{ 255 }) == mo_args);
+		REQUIRE(member_index<false>(member::global{ 25 }) == mo_global_16);
+		REQUIRE(member_index<false>(member::global{ 6531 }) == mo_global_16);
+		REQUIRE(member_index<false>(member::local{ 25 }) == mo_local_16);
+		REQUIRE(member_index<false>(member::local{ 6531 }) == mo_local_16);
+		REQUIRE(member_index<false>(member::resource{ 25 }) == mo_resource_16);
+		REQUIRE(member_index<false>(member::resource{ 6531 }) == mo_resource_16);
+	}
+
+	SECTION("optimized")
+	{
+		REQUIRE(member_index<true>(member::tos{}) == mo_tos);
+		REQUIRE(member_index<true>(member::args{ 255 }) == mo_args);
+		REQUIRE(member_index<true>(member::global{ 25 }) == mo_global_8);
+		REQUIRE(member_index<true>(member::global{ 6531 }) == mo_global_16);
+		REQUIRE(member_index<true>(member::local{ 25 }) == mo_local_8);
+		REQUIRE(member_index<true>(member::local{ 6531 }) == mo_local_16);
+		REQUIRE(member_index<true>(member::resource{ 25 }) == mo_resource_8);
+		REQUIRE(member_index<true>(member::resource{ 6531 }) == mo_resource_16);
+	}
 }
