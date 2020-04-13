0ce97cc5cf31bcfb6904b755c3de927aa1e3933e
terrakuh <greybley@yahoo.de>
2020-04-13 14:27:06 +0200

    Update generateVM.py

--- generateVM.py
+++ generateVM.py
@@ -4,48 +4,75 @@ CODE = 2
 
 arg_options = {
     "mso": [
-        ("tos", "auto& p{} = stack.tos();"),
-        ("args", "auto& p{} = stack.arg_at(ip.read<std::uint8_t>());"),
-        ("global_16", "auto& p{} = stack.local_at(ip.read<std::uint16_t>());"),
-        ("local_16", "auto& p{} = stack.local_at(ip.read<std::uint16_t>());"),
-        ("resource_16", "auto& p{} = stack.local_at(ip.read<std::uint16_t>());"),
-        ("global_8", "auto& p{} = stack.local_at(ip.read<std::uint8_t>());"),
-        ("local_8", "auto& p{} = stack.local_at(ip.read<std::uint8_t>());"),
-        ("resource_8", "auto& p{} = stack.local_at(ip.read<std::uint8_t>());")
+        ("tos", "stack.tos()"),
+        ("args", "stack.arg_at(ip.read<std::uint8_t>())"),
+        ("global_16", "stack.local_at(ip.read<std::uint16_t>())"),
+        ("local_16", "stack.local_at(ip.read<std::uint16_t>())"),
+        ("resource_16", "stack.local_at(ip.read<std::uint16_t>())"),
+        ("global_8", "stack.local_at(ip.read<std::uint8_t>())"),
+        ("local_8", "stack.local_at(ip.read<std::uint8_t>())"),
+        ("resource_8", "stack.local_at(ip.read<std::uint8_t>())")
     ],
     "mdo": [
-        ("tos", "auto& p{} = stack.push();"),
-        ("global_16", "auto& p{} = stack.local_at(ip.read<std::uint16_t>());"),
-        ("local_16", "auto& p{} = stack.local_at(ip.read<std::uint16_t>());"),
-        ("global_8", "auto& p{} = stack.local_at(ip.read<std::uint8_t>());"),
-        ("local_8", "auto& p{} = stack.local_at(ip.read<std::uint8_t>());")
+        ("tos", "stack.push()"),
+        ("global_16", "stack.local_at(ip.read<std::uint16_t>())"),
+        ("local_16", "stack.local_at(ip.read<std::uint16_t>())"),
+        ("global_8", "stack.local_at(ip.read<std::uint8_t>())"),
+        ("local_8", "stack.local_at(ip.read<std::uint8_t>())")
     ],
     "co": [
-        ("int_8", "const auto p{} = ip.read<std::int8_t>();"),
-        ("int_32", "const auto p{} = ip.read<std::int32_t>();"),
-        ("int_64", "const auto p{} = ip.read<std::int64_t>();"),
-        ("double", "const auto p{} = ip.read<double>();")
+        ("int_8", "ip.read<std::int8_t>()"),
+        ("int_32", "ip.read<std::int32_t>()"),
+        ("int_64", "ip.read<std::int64_t>()"),
+        ("double", "ip.read<double>()")
     ]
 }
 opcodes = [
-    ("oc_instantiate", ("mdo", "co"),
-     "token.set({0}, creator::create({1}).to<bia::member::member>());"),
-    ("oc_invoke", ("mdo", "mso"), """const auto parameter_count = ip.read<std::uint8_t>();
-token.set({0}, member_pointer({1})->invoke(stack.frame(), parameter_count));
-stack.drop(parameter_count);"""),
+    ("oc_instantiate", ("co", "mdo"), """const auto constant = {0};
 
-    ("oc_refer", ("mdo", "mso"), "token.set({0}, {1});"),
-    ("oc_clone", ("mdo", "mso"), """const auto ptr = member_pointer({1});
-if (ptr->flags() & flag::flag_clone_is_copy) {{
-    token.set({0}, ptr->copy());
+token.set({1}, creator::create(constant).to<bia::member::member>());
+
+break;"""),
+
+    ("oc_invoke", ("mso", "mdo"), """const auto parameter_count = ip.read<std::uint8_t>();
+auto result = member_pointer({0})->invoke(stack.frame(parameter_count), parameter_count);
+
+stack.drop(parameter_count);
+token.set({1}, std::move(result));
+
+break;"""),
+
+
+    ("oc_refer", ("mso", "mdo"), """auto& src = {0};
+
+token.set({1}, src);
+
+break;"""),
+
+    ("oc_clone", ("mso", "mdo"), """const auto src = member_pointer({0});
+
+if (src->flags() & flag::flag_clone_is_copy) {{
+    token.set({1}, src->copy());
 }} else {{
-    token.set({0}, ptr);
-}}"""),
-    ("oc_copy", ("mdo", "mso"), "token.set({0}, member_pointer({1})->copy());"),
+    token.set({1}, src);
+}}
+
+break;"""),
+
+    ("oc_copy", ("mso", "mdo"),  """const auto src = member_pointer({0});
+
+token.set({1}, src->copy());
+
+break;"""),
+
 
     ("oc_invoke_void", ("mso",), """const auto parameter_count = ip.read<std::uint8_t>();
-member_pointer({0})->invoke(stack.frame(), parameter_count);
-stack.drop(parameter_count);"""),
+
+member_pointer({0})->invoke(stack.frame(parameter_count), parameter_count);
+stack.drop(parameter_count);
+
+break;"""),
+
     ("oc_test", ("mso",), "test_register = member_pointer({0})->test();"),
 
     ("oc_return_void", tuple(), "return;"),
@@ -70,11 +97,7 @@ for opcode in opcodes:
         else:
             print("case ({}{}): {{".format(
                 opcode[NAME], args_template.format(*(i[0] for i in args))))
-            for index, i in enumerate(args):
-                print(i[1].format(index))
-            print(opcode[CODE].format(*("p{}".format(i)
-                                        for i in range(len(args)))))
-            print("break;\n}")
+            print(opcode[CODE].format(*(i[1] for i in args)), end="\n}\n")
 
     rec(opcode[ARGS])
     #case = "case ({}{}):".format(opcode[NAME], args_template.format([i() for i in args]))
