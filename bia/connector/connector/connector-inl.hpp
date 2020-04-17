0d3fb22d63a35816d357d33a9794fc2629fbffcf
terrakuh <greybley@yahoo.de>
2020-04-17 17:04:23 +0200

    Add static connector

--- bia/connector/connector/connector-inl.hpp
+++ bia/connector/connector/connector-inl.hpp
@@ -2,10 +2,45 @@
 #define BIA_CONNECTOR_CONNECTOR_INL_HPP_
 
 #include <connector/connector.hpp>
+#include <creator/creator.hpp>
+#include <util/type_traits/int_maker.hpp>
+#include <gc/gcable.hpp>
+#include <gc/stack_view.hpp>
 
 namespace bia {
 namespace connector {
-	
+
+template<std::size_t... Indices>
+using parameter_indices = util::type_traits::int_container<std::size_t, Indices...>;
+template<std::size_t Count>
+using parameter_index_maker = util::type_traits::int_sequencer<std::size_t, 0, Count>;
+
+template<typename Return, typename... Args, std::size_t... Indices>
+inline gc::gcable<member::member> connect_static(Return (*function)(Args...), gc::stack_view& stack,
+                                                 parameter_indices<Indices...>)
+{
+	return creator::create(function(stack.arg_at(Indices)...)).template to<member::member>();
+}
+
+template<typename... Args, std::size_t... Indices>
+inline gc::gcable<member::member> connect_static(void (*function)(Args...), gc::stack_view& stack,
+                                                 parameter_indices<Indices...>)
+{
+	function(stack.arg_at(Indices)...);
+
+	return {};
+}
+
+template<typename Return, typename... Args>
+inline gc::gcable<member::member> connect_static(Return (*function)(Args...), gc::stack_view& stack,
+                                                 std::size_t parameter_count)
+{
+	if (parameter_count != sizeof...(Args)) {
+		throw;
+	}
+
+	return connect_static(function, stack, parameter_index_maker<sizeof...(Args)>::value);
+}
 
 } // namespace connector
 } // namespace bia
--- bia/connector_interface/connector/connector.hpp
+++ bia/connector_interface/connector/connector.hpp
@@ -1,11 +1,21 @@
 #ifndef BIA_CONNECTOR_INTERFACE_CONNECTOR_HPP_
 #define BIA_CONNECTOR_INTERFACE_CONNECTOR_HPP_
 
-#include <member/member.hpp>
+#include <cstdint>
 
 namespace bia {
+namespace gc {
+
+template<typename>
+class gcable;
+class stack_view;
+
+} // namespace gc
 namespace connector {
 
+template<typename Return, typename... Args>
+gc::gcable<member::member> connect_static(Return (*function)(Args...), gc::stack_view& stack,
+                                          std::size_t parameter_count);
 
 } // namespace connector
 } // namespace bia
