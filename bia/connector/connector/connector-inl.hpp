204e3c9ba166bdef6a29737e6e8fdb86f055176a
terrakuh <greybley@yahoo.de>
2020-04-18 14:32:06 +0200

    Update connector-inl.hpp

--- bia/connector/connector/connector-inl.hpp
+++ bia/connector/connector/connector-inl.hpp
@@ -3,6 +3,7 @@
 
 #include <connector/connector.hpp>
 #include <creator/creator.hpp>
+#include <exception/nullpointer.hpp>
 #include <gc/gcable.hpp>
 #include <gc/stack_view.hpp>
 #include <member/cast.hpp>
@@ -17,18 +18,29 @@ using parameter_indices = util::type_traits::int_container<std::size_t, Indices.
 template<std::size_t Count>
 using parameter_index_maker = util::type_traits::int_sequencer<std::size_t, 0, Count>;
 
+inline member::member& not_null(gc::stack_view::element_type& element)
+{
+	if (const auto ptr = element.get()) {
+		return *ptr;
+	}
+
+	BIA_THROW(exception::nullpointer, "nullpointer argument");
+}
+
 template<typename Return, typename... Args, std::size_t... Indices>
 inline gc::gcable<member::member> connect_static(Return (*function)(Args...), gc::stack_view& stack,
                                                  parameter_indices<Indices...>)
 {
-	return creator::create(function(stack.arg_at(Indices)...)).template to<member::member>();
+	return creator::create(
+	           function(std::forward<Args>(member::cast<Args>(not_null(stack.arg_at(Indices))))...))
+	    .template to<member::member>();
 }
 
 template<typename... Args, std::size_t... Indices>
 inline gc::gcable<member::member> connect_static(void (*function)(Args...), gc::stack_view& stack,
                                                  parameter_indices<Indices...>)
 {
-	function(std::forward<Args>(member::cast<Args>(stack.arg_at(Indices)))...);
+	function(std::forward<Args>(member::cast<Args>(not_null(stack.arg_at(Indices))))...);
 
 	return {};
 }
