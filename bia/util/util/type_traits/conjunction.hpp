964d4ef59637a987e33e5bce110a0e241e6a9dcd
terrakuh <greybley@yahoo.de>
2020-04-18 13:54:31 +0200

    Update conjunction.hpp

--- bia/util/util/type_traits/conjunction.hpp
+++ bia/util/util/type_traits/conjunction.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef BIA_UTIL_TYPE_TRAITS_HPP_
+#define BIA_UTIL_TYPE_TRAITS_HPP_
 
 #include <type_traits>
 
@@ -15,9 +16,12 @@ struct conjunction<B> : std::conditional<B, std::true_type, std::false_type>::ty
 {};
 
 template<bool B0, bool... B>
-struct conjunction<B0, B...> : std::conditional<B0 && conjunction<B...>::value, std::true_type, std::false_type>::type
+struct conjunction<B0, B...>
+    : std::conditional<B0 && conjunction<B...>::value, std::true_type, std::false_type>::type
 {};
 
 } // namespace type_traits
 } // namespace util
 } // namespace bia
+
+#endif
