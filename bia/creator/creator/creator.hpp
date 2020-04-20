770868750bfb3082f0d8ceea5088813e31e4ee96
terrakuh <greybley@yahoo.de>
2020-04-20 13:28:05 +0200

    Add string and static function creator

--- bia/creator/creator/creator.hpp
+++ bia/creator/creator/creator.hpp
@@ -1,8 +1,12 @@
 #ifndef BIA_CREATOR_CREATOR_HPP_
 #define BIA_CREATOR_CREATOR_HPP_
 
+#include <cstring>
 #include <gc/gc.hpp>
+#include <member/function/static_.hpp>
 #include <member/native/integer.hpp>
+#include <member/native/string.hpp>
+#include <string>
 #include <type_traits>
 
 namespace bia {
@@ -24,7 +28,24 @@ inline typename std::enable_if<std::is_floating_point<typename std::decay<T>::ty
     create(T&& value)
 {
 	return {};
-	//return gc::gc::active_gc()->construct<member::native::int_member>(std::forward<T>(value));
+	// return gc::gc::active_gc()->construct<member::native::int_member>(std::forward<T>(value));
+}
+
+template<typename Return, typename... Args>
+inline gc::gcable<member::function::static_<Return, Args...>> create(Return (*function)(Args...))
+{
+	return gc::gc::active_gc()->construct<member::function::static_<Return, Args...>>(function);
+}
+
+inline gc::gcable<member::native::string> create(const std::string& value)
+{
+	const auto gc  = gc::gc::active_gc();
+	const auto str = static_cast<char*>(gc->allocate(value.size() + 1).release());
+	auto mem       = gc->construct<member::native::string>(str);
+
+	std::memcpy(str, value.c_str(), value.size() + 1);
+
+	return mem;
 }
 
 } // namespace creator
