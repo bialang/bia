f7501cf26ef2d50de19f5f6f96f08019fd04c6a5
terrakuh <greybley@yahoo.de>
2020-04-25 12:01:28 +0200

    Clean up compiler::jump_manager

--- bia/compiler/compiler/jump_manager.hpp
+++ bia/compiler/compiler/jump_manager.hpp
@@ -15,6 +15,7 @@ public:
 	typedef std::ostream::pos_type pos_type;
 	typedef std::int32_t offset_type;
 
+	/** the jump type */
 	enum class type
 	{
 		unconditional,
@@ -22,6 +23,7 @@ public:
 		if_false
 	};
 
+	/** the jump destination */
 	enum class destination
 	{
 		end,
@@ -29,66 +31,22 @@ public:
 		start
 	};
 
-	jump_manager(util::not_null<std::ostream*> output) noexcept : _output(*output)
-	{}
-	void mark(destination destination)
-	{
-		const auto cpos = _output.tellp();
-
-		switch (destination) {
-		case destination::next: {
-			BIA_EXPECTS(_last.pos != pos_type{ -1 });
-
-			_correct_jump(_last, cpos);
-
-			_last = { { -1 } };
-
-			break;
-		}
-		case destination::end: {
-			for (const auto& i : _end_jumps) {
-				_correct_jump(i, cpos);
-			}
-
-			break;
-		}
-		case destination::start: {
-			_start = cpos;
-
-			return;
-		}
-		}
-
-		_output.seekp(cpos);
-	}
-	void jump(type type, destination destination)
-	{
-		const pos_type cpos = _output.tellp();
-
-		switch (destination) {
-		case destination::next: {
-			BIA_EXPECTS(_last.pos == pos_type{ -1 });
-
-			_last = { cpos, type };
-
-			break;
-		}
-		case destination::end: {
-			_end_jumps.push_back({ cpos, type });
-
-			break;
-		}
-		case destination::start: {
-			BIA_EXPECTS(_start != pos_type{ -1 });
-
-			_write_jump(type, _start - cpos - _jump_size);
-
-			return;
-		}
-		}
-
-		_write_jump(type, 0);
-	}
+	jump_manager(util::not_null<std::ostream*> output) noexcept;
+	/**
+	 * Marks the current position as a destination for jumps.
+	 *
+	 * @note may update the offset of previously set jumps
+	 *
+	 * @param destination the type of the destination
+	 */
+	void mark(destination destination);
+	/**
+	 * Writes a jump to a specific destination.
+	 *
+	 * @param type the type of the jump
+	 * @param destination the jump destination
+	 */
+	void jump(type type, destination destination);
 
 private:
 	struct info
@@ -104,28 +62,8 @@ private:
 	info _last{ { -1 } };
 	std::vector<info> _end_jumps;
 
-	void _correct_jump(const info& info, pos_type cpos)
-	{
-		_output.seekp(info.pos);
-		_write_jump(info.type, cpos - info.pos - _jump_size);
-	}
-	void _write_jump(type type, offset_type offset)
-	{
-		switch (type) {
-		case type::unconditional:
-			bytecode::writer::instruction(_output).write<false, bytecode::oc_jump>(offset);
-
-			break;
-		case type::if_true:
-			bytecode::writer::instruction(_output).write<false, bytecode::oc_jump_true>(offset);
-
-			break;
-		case type::if_false:
-			bytecode::writer::instruction(_output).write<false, bytecode::oc_jump_false>(offset);
-
-			break;
-		}
-	}
+	void _correct_jump(const info& info, pos_type cpos);
+	void _write_jump(type type, offset_type offset);
 };
 
 } // namespace compiler
