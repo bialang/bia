3ba6b9645a66c76a95628d2fb2f067a8734aafb4
terrakuh <greybley@yahoo.de>
2020-06-04 11:16:55 +0200

    Add jumps to if statement

--- bia/compiler/compiler/elve/if_.hpp
+++ bia/compiler/compiler/elve/if_.hpp
@@ -1,6 +1,7 @@
 #ifndef BIA_COMPILER_ELVE_IF_HPP_
 #define BIA_COMPILER_ELVE_IF_HPP_
 
+#include "../jump_manager.hpp"
 #include "batch.hpp"
 #include "expression.hpp"
 #include "present.hpp"
@@ -16,18 +17,25 @@ inline tokens_type if_(present present, tokens_type tokens)
 	BIA_EXPECTS(!tokens.empty() &&
 	            static_cast<token::type>(tokens.data()->value.index()) == token::type::keyword);
 
+	jump_manager jumper{ &present.writer.output() };
+
 	tokens = expression(present, tokens.subspan(1), bytecode::member::tos{}, -1);
 
 	present.writer.write<true, bytecode::oc_test>(bytecode::member::tos{});
 	present.writer.write<true, bytecode::oc_drop>(1);
-	present.writer.write<false, bytecode::oc_jump_false>(std::int32_t{ 0 });
+	jumper.jump(jump_manager::type::if_false, jump_manager::destination::next);
 
 	// process batch
 	tokens = batch(present, tokens);
 
+	jumper.jump(jump_manager::type::unconditional, jump_manager::destination::end);
+	jumper.mark(jump_manager::destination::next);
+
 	// do all else if
 
 	// check for else
+
+	jumper.mark(jump_manager::destination::end);
 
 	return tokens;
 }
