f858ab455cbeb35d5afcc4b6ec2e89c6ae2c90d7
terrakuh <greybley@yahoo.de>
2020-06-14 00:56:45 +0200

    Update number token

--- bia/tokenizer/token/parse/number.hpp
+++ bia/tokenizer/token/parse/number.hpp
@@ -4,43 +4,148 @@
 #include "../parameter.hpp"
 
 #include <bia/exception/syntax_error.hpp>
+#include <bia/util/variant.hpp>
 #include <limits>
 
 namespace bia {
 namespace tokenizer {
 namespace token {
 namespace parse {
+namespace detail {
+
+inline bool integer_add(token::int_type& value, string::encoding::code_point_type cp, int base)
+{
+	BIA_EXPECTS(cp >= '0' && cp <= '9');
+
+	const auto val = cp - '0';
+
+	// limit
+	if (std::numeric_limits<token::int_type>::max() / 10 < value ||
+	    std::numeric_limits<token::int_type>::max() - value * 10 < val) {
+		return false;
+	}
+
+	value = value * 10 + val;
+
+	return true;
+}
+
+inline bool floating_point_add(double& value, string::encoding::code_point_type cp, int base)
+{
+	value += (cp - '0') / 10.0;
+
+	return true;
+}
+
+} // namespace detail
 
 inline exception::syntax_details number(parameter& parameter)
 {
-	token::int_type value = 0;
-	auto parsed           = false;
+	enum class state
+	{
+		start,
+		decimal,
+		zero,
+		octal,
+		hex_begin,
+		hex,
+		binary_begin,
+		binary,
+		floating_point,
+	};
+
+	auto s = state::start;
+	util::variant<token::int_type, double> value{ 0 };
 
 	while (true) {
 		const auto pos = parameter.input.tellg();
 		const auto cp  = parameter.encoder.read(parameter.input);
 
-		if (cp >= '0' && cp <= '9') {
-			const auto val = cp - '0';
-
-			// limit
-			if (std::numeric_limits<token::int_type>::max() / 10 < value ||
-			    std::numeric_limits<token::int_type>::max() - value * 10 < val) {
-				return { pos, "integer overflow" };
-			}
-
-			value  = value * 10 + val;
-			parsed = true;
-		} else {
-			if (!parsed) {
-				return { pos, "invalid number" };
-			}
-
-			parameter.input.seekg(pos);
-			parameter.bundle.add(token{ token::int_type{ value } });
-
-			return {};
+		// consume ' as whitespace
+		if (s != state::start && cp == '\'') {
+			continue;
 		}
+
+		switch (s) {
+		case state::start: {
+			if (cp == '0') {
+				s = state::zero;
+			} else if (cp >= '1' && cp <= '9') {
+				s              = state::decimal;
+				value.get<0>() = cp - '0';
+			} else {
+				return { pos, "unexpected start of number" };
+			}
+
+			break;
+		}
+		case state::decimal: {
+			if (cp >= '0' && cp <= '9') {
+				if (!detail::integer_add(value.get<0>(), cp, 10)) {
+					return { pos, "integer overflow" };
+				}
+			} else if (cp == '.') {
+				s = state::floating_point;
+
+				value.emplace<1>(value.get<0>());
+			} else {
+				goto gt_end;
+			}
+
+			break;
+		}
+		case state::zero: {
+			if (cp == 'x' || cp == 'X') {
+				s = state::hex_begin;
+			} else if (cp == 'b' || cp == 'B') {
+				s = state::binary_begin;
+			} else if (cp >= '0' && cp <= '7') {
+				s              = state::octal;
+				value.get<0>() = cp - '0';
+			} else if (cp == '.') {
+				s = state::floating_point;
+
+				value.emplace<1>(value.get<0>());
+			} else {
+				goto gt_end;
+			}
+
+			break;
+		}
+		case state::octal: {
+			if (cp >= '0' && cp <= '7') {
+				if (!detail::integer_add(value.get<0>(), cp, 7)) {
+					return { pos, "integer overflow" };
+				}
+			} else {
+				goto gt_end;
+			}
+
+			break;
+		}
+		case state::floating_point: {
+			if (cp >= '0' && cp <= '9') {
+				if (!detail::floating_point_add(value.get<1>(), cp, 10)) {
+					return { pos, "floating point overflow" };
+				}
+			}
+
+			break;
+		}
+		default: BIA_IMPLEMENTATION_ERROR("not implemented");
+		}
+
+	gt_end:;
+		parameter.input.seekg(pos);
+
+		// double value
+		if (value.index()) {
+			parameter.bundle.add({ value.get<1>() });
+		} else {
+			parameter.bundle.add({ value.get<0>() });
+		}
+
+		return {};
 	}
 }
 
