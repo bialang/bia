894bdd1c55303383917167df40c0a6eb52c51295
terrakuh <greybley@yahoo.de>
2020-06-24 20:03:20 +0200

    Add functor info

--- bia/util/type_traits/method_info.hpp
+++ bia/util/type_traits/method_info.hpp
@@ -2,6 +2,7 @@
 #define BIA_UTIL_TYPE_TRAITS_METHOD_INFO_HPP_
 
 #include "type_maker.hpp"
+#include "void_type.hpp"
 
 namespace bia {
 namespace util {
@@ -37,6 +38,19 @@ struct method_info<Return (Class::*)(Args...) const>
 	constexpr static auto is_const  = true;
 	constexpr static auto arg_count = sizeof...(Args);
 	constexpr static type_container<Args...> args{};
+};
+
+template<typename Functor, typename = void_type<>>
+struct functor_info
+{
+	constexpr static auto is_functor = false;
+};
+
+template<typename Functor>
+struct functor_info<Functor, void_type<decltype(&Functor::operator())>>
+    : method_info<decltype(&Functor::operator())>
+{
+	constexpr static auto is_functor = true;
 };
 
 } // namespace type_traits
