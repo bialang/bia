5be239b0d1a5ca46713c2d3676631bf032a6c61c
terrakuh <greybley@yahoo.de>
2020-06-24 18:32:31 +0200

    Add const methods

--- bia/member/connector.hpp
+++ bia/member/connector.hpp
@@ -30,6 +30,10 @@ template<typename Class, typename Return, typename... Args>
 gc::gcable<member::member> connect_method(Class& instance, Return (Class::*function)(Args...),
                                           parameters_type params);
 
+template<typename Class, typename Return, typename... Args>
+gc::gcable<member::member> connect_method(const Class& instance, Return (Class::*function)(Args...) const,
+                                          parameters_type params);
+
 } // namespace connector
 } // namespace bia
 
