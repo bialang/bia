4b4b85cb3cb8ac56eb8de00f28bc7fc823dc7bb9
terrakuh <greybley@yahoo.de>
2020-07-19 09:59:21 +0200

    Fix math functions

--- bia/bsl/math.cpp
+++ bia/bsl/math.cpp
@@ -9,57 +9,79 @@
 
 using namespace bia::bsl;
 
-inline typename bia::member::native::integer::int_type sum(bia::connector::parameters_type params)
+inline bia::gc::gcable<bia::member::member> sum(bia::connector::parameters_type params)
 {
-	typedef typename bia::member::native::integer::int_type int_type;
-
-	int_type s = 0;
+	bia::gc::gcable<bia::member::member> s;
 
 	for (auto i : params.positionals()) {
-		if (i) {
-			s += bia::member::cast::cast<int_type>(*i);
+		if (!s.peek()) {
+			s = i;
+		} else if (i) {
+			s = s.peek()->operation(*i, bia::member::infix_operator::addition);
 		}
 	}
 
 	return s;
 }
 
-inline typename bia::member::native::integer::int_type max(bia::connector::parameters_type params)
+inline bia::gc::gcable<bia::member::member> max(bia::connector::parameters_type params)
 {
-	typedef typename bia::member::native::integer::int_type int_type;
-
-	int_type m = 0;
+	bia::gc::gcable<bia::member::member> m;
 
 	for (auto i : params.positionals()) {
-		if (i) {
-			const auto tmp = bia::member::cast::cast<int_type>(*i);
-
-			if (tmp > m) {
-				m = tmp;
-			}
+		if (!m.peek() || (i && m.peek()->test(bia::member::test_operator::less, *i))) {
+			m = i;
 		}
 	}
 
 	return m;
 }
 
-inline typename bia::member::native::integer::int_type min(bia::connector::parameters_type params)
+inline bia::gc::gcable<bia::member::member> min(bia::connector::parameters_type params)
 {
-	typedef typename bia::member::native::integer::int_type int_type;
-
-	int_type m = 0;
+	bia::gc::gcable<bia::member::member> m;
 
 	for (auto i : params.positionals()) {
-		if (i) {
-			const auto tmp = bia::member::cast::cast<int_type>(*i);
-
-			if (tmp < m) {
-				m = tmp;
-			}
+		if (!m.peek() || (i && m.peek()->test(bia::member::test_operator::greater, *i))) {
+			m = i;
 		}
 	}
 
 	return m;
+}
+
+inline bia::gc::gcable<bia::member::member> average(bia::connector::parameters_type params)
+{
+	bia::gc::gcable<bia::member::member> s;
+
+	for (auto i : params.positionals()) {
+		if (!s.peek()) {
+			s = i;
+		} else if (i) {
+			s = s.peek()->operation(*i, bia::member::infix_operator::addition);
+		}
+	}
+
+	if (const auto ptr = s.peek()) {
+		const bia::member::native::integer count{ params.size() };
+
+		return ptr->operation(count, bia::member::infix_operator::division);
+	}
+
+	return s;
+}
+
+inline bia::gc::gcable<bia::member::member> absolute(bia::connector::parameters_type params)
+{
+	if (params.size() != 1 || !params[0]) {
+		BIA_IMPLEMENTATION_ERROR("not implemented");
+	}
+
+	const bia::member::native::integer zero{ 0 };
+
+	return params[0]->test(bia::member::test_operator::less, zero)
+	           ? params[0]->self_operation(bia::member::self_operator::unary_minus)
+	           : params[0];
 }
 
 math::math(gc::gc& gc) : _symbols{ _init(gc) }
@@ -98,6 +120,8 @@ bia::member::native::dict* math::_init(gc::gc& gc)
 	put_function(gc, *dict, "sum", &sum);
 	put_function(gc, *dict, "max", &max);
 	put_function(gc, *dict, "min", &min);
+	put_function(gc, *dict, "avg", &average);
+	put_function(gc, *dict, "abs", &absolute);
 	put_function(gc, *dict, "sqrt", static_cast<double (*)(double)>(&std::sqrt));
 	put_function(gc, *dict, "cbrt", static_cast<double (*)(double)>(&std::cbrt));
 	put_function(gc, *dict, "pow", static_cast<double (*)(double, double)>(&std::pow));
@@ -120,7 +144,7 @@ bia::member::native::dict* math::_init(gc::gc& gc)
 	put_function(gc, *dict, "round", static_cast<double (*)(double)>(&std::round));
 	put_function(gc, *dict, "hypot", static_cast<double (*)(double, double)>(&std::hypot));
 	put_gcable(gc, *dict, "pi", creator::create(3.141592653589793238462643383279502884197169399375105820974));
-	put_gcable(gc, *dict, "pi", creator::create(2.718281828459045235360287471352662497757247093699959574966));
+	put_gcable(gc, *dict, "e", creator::create(2.718281828459045235360287471352662497757247093699959574966));
 
 	return dict;
 }
