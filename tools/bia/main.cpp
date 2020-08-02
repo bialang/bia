d3b8162e734f2b23b450008c5462f3cea2f844e9
terrakuh <greybley@yahoo.de>
2020-08-02 14:15:40 +0200

    Update bia tool

--- tools/bia/main.cpp
+++ tools/bia/main.cpp
@@ -8,16 +8,20 @@ int main(int argc, char** argv)
 		"Bia command line tool used for executing, compiling, packaging and installing scripts/modules"
 	};
 
-	app.set_help_all_flag("--help-all", "Expand help all");
-	app.set_help_flag("-h,--help", "Expand quick help");
+	app.set_help_all_flag("--help-all", "expand help all");
+	app.set_help_flag("-h,--help", "expand quick help");
 
 	auto run = app.add_subcommand("run", "run a given source file");
 
 	std::string file;
 	std::vector<std::string> args;
+	auto no_bsl = false;
+	auto no_bel = false;
 
 	run->add_option("source", file, "the source file")->required()->check(CLI::ExistingFile);
 	run->add_option("args", args, "the remaining arguments that are passed to the script");
+	run->add_flag("--no-bsl", no_bsl, "disable the use of the standard library");
+	run->add_flag("--no-bel", no_bel, "disable the use of the extended library");
 
 	app.require_subcommand(0, 1);
 
@@ -33,7 +37,9 @@ int main(int argc, char** argv)
 			arg_ptrs.push_back(i.c_str());
 		}
 
-		engine.use_bsl({ arg_ptrs.data(), arg_ptrs.data() + arg_ptrs.size() });
+		if (!no_bsl) {
+			engine.use_bsl({ arg_ptrs.data(), arg_ptrs.data() + arg_ptrs.size() });
+		}
 
 		std::ifstream f{ file, std::ios::in | std::ios::binary };
 
